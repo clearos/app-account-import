f9f48d8eafb9203e8e05954b9ed07f5c63e9eada
clearostranslations <translations@clearfoundation.com>
2014-11-21 19:35:59 -0500

    Updated translations via Pootle

--- language/ru_RU/account_import_lang.php
+++ language/ru_RU/account_import_lang.php
@@ -3,19 +3,19 @@
 // http://www.clearfoundation.com/docs/developer/translations/
 
 $lang['account_import_app_description'] = 'The Account Import app performs bulk import of accounts using an uploaded file in comma separated values (CSV) format.';
-$lang['account_import_app_name'] = 'Account Import';
-$lang['account_import_clear'] = 'Clear';
-$lang['account_import_csv_file'] = 'CSV File';
-$lang['account_import_csv_file_not_found'] = 'CSV file not found.';
-$lang['account_import_download_csv_template'] = 'Download CSV Template';
-$lang['account_import_import_already_in_progress'] = 'Import is already in progress.';
-$lang['account_import_import_complete'] = 'Import complete.';
-$lang['account_import_import_log'] = 'Import Log';
-$lang['account_import_import_users'] = 'Import Users';
-$lang['account_import_importing_user'] = 'Importing user';
-$lang['account_import_no_data_found'] = 'No data found.';
-$lang['account_import_no_entries'] = 'No import entries were found.';
-$lang['account_import_number_of_records'] = 'Number of Records';
-$lang['account_import_start_import'] = 'Start Import';
-$lang['account_import_unable_to_determine_running_state'] = 'Unable to determine if import is running.';
-$lang['account_import_upload_csv_file'] = 'Upload CSV File';
+$lang['account_import_app_name'] = 'Импорт учетной записи';
+$lang['account_import_clear'] = 'Очистить';
+$lang['account_import_csv_file'] = 'Файл CSV';
+$lang['account_import_csv_file_not_found'] = ' CSV файл не найден';
+$lang['account_import_download_csv_template'] = 'Скачать CSV шаблон';
+$lang['account_import_import_already_in_progress'] = 'Импорт уже идет.';
+$lang['account_import_import_complete'] = 'Импортирование завершено.';
+$lang['account_import_import_log'] = 'Импорт журнала';
+$lang['account_import_import_users'] = 'Импорт пользователей';
+$lang['account_import_importing_user'] = 'Импорт пользователя';
+$lang['account_import_no_data_found'] = 'Нет данных.';
+$lang['account_import_no_entries'] = 'Никакие записи импорта не были найдены.';
+$lang['account_import_number_of_records'] = 'Количество записей';
+$lang['account_import_start_import'] = 'Начать импорт';
+$lang['account_import_unable_to_determine_running_state'] = 'Невозможно определить, если импорт работает.';
+$lang['account_import_upload_csv_file'] = 'Загрузить CSV файл';
