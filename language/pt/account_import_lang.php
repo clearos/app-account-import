a89e58fbd7346121ce9b8ffd7c64845a45ba901a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:38:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pt/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'O aplicativo conta Import executa importação em massa de contas usando um arquivo carregado em valores separados por vírgulas formato (CSV).';
+$lang['account_import_app_name'] = 'Importação conta';
+$lang['account_import_clear'] = 'Claro';
+$lang['account_import_csv_file'] = 'Arquivo CSV';
+$lang['account_import_csv_file_not_found'] = 'arquivo CSV não foi encontrado.';
+$lang['account_import_download_csv_template'] = 'Baixar modelo CSV';
+$lang['account_import_import_already_in_progress'] = 'Importação já está em andamento.';
+$lang['account_import_import_complete'] = 'Importação completa.';
+$lang['account_import_import_log'] = 'Import Log';
+$lang['account_import_import_users'] = 'importar usuários';
+$lang['account_import_importing_user'] = 'utilizador importador';
+$lang['account_import_no_data_found'] = 'Nenhum dado encontrado.';
+$lang['account_import_no_entries'] = 'Nenhuma entrada de importação foram encontrados.';
+$lang['account_import_number_of_records'] = 'Número de registros';
+$lang['account_import_start_import'] = 'Iniciar importação';
+$lang['account_import_tooltip_csv'] = 'Garantir o seu arquivo CSV está formatado corretamente - ele deve ter valores entre aspas e separados por uma vírgula ( "value1", "value2" etc.).';
+$lang['account_import_unable_to_determine_running_state'] = 'Não foi possível determinar se a importação está em execução.';
+$lang['account_import_upload_csv_file'] = 'Enviar arquivo CSV';
