97cd5f21d3deec6c005327cfd85cfb39d3a79fba
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:15:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'ಖಾತೆ ಆಮದು ಅಪ್ಲಿಕೇಶನ್ ಅಲ್ಪವಿರಾಮ ಬೇರ್ಪಡಿಸಿದ ಮೌಲ್ಯಗಳು (CSV) ರೂಪದಲ್ಲಿ ಒಂದು ಅಪ್ಲೋಡ್ ಫೈಲ್ ಬಳಸಿ ಖಾತೆಗಳ ಬೃಹತ್ ಆಮದು ನಿರ್ವಹಿಸುತ್ತದೆ.';
+$lang['account_import_app_name'] = 'ಖಾತೆ ಆಮದು';
+$lang['account_import_clear'] = 'ಸ್ಪಷ್ಟ';
+$lang['account_import_csv_file'] = 'CSV ಕಡತ';
+$lang['account_import_csv_file_not_found'] = 'CSV ಫೈಲ್ ಕಂಡುಬಂದಿಲ್ಲ.';
+$lang['account_import_download_csv_template'] = 'ಜಿನ್ಯೂಮರಿಕ್ CSV ಟೆಂಪ್ಲೇಟು';
+$lang['account_import_import_already_in_progress'] = 'ಆಮದು ಈಗಾಗಲೇ ಪ್ರಗತಿಯಲ್ಲಿದೆ.';
+$lang['account_import_import_complete'] = 'ಆಮದು ಪೂರ್ಣಗೊಂಡಿದೆ.';
+$lang['account_import_import_log'] = 'ಆಮದು ಲಾಗ್';
+$lang['account_import_import_users'] = 'ಆಮದು ಬಳಕೆದಾರರು';
+$lang['account_import_importing_user'] = 'ಆಮದು ಬಳಕೆದಾರ';
+$lang['account_import_no_data_found'] = 'ಯಾವುದೇ ಡೇಟಾ ಕಂಡುಬಂದಿಲ್ಲ.';
+$lang['account_import_no_entries'] = 'ಯಾವುದೇ ಆಮದು ನಮೂದುಗಳನ್ನು ಕಂಡುಬಂದಿಲ್ಲ.';
+$lang['account_import_number_of_records'] = 'ರೆಕಾರ್ಡ್ಗಳ ಸಂಖ್ಯೆಯನ್ನು';
+$lang['account_import_start_import'] = 'ಪ್ರಾರಂಭಿಸಿ ಆಮದು';
+$lang['account_import_tooltip_csv'] = 'ನಿಮ್ಮ CSV ಫೈಲ್ ಸರಿಯಾಗಿ ಫಾರ್ಮ್ಯಾಟ್ ಖಚಿತಪಡಿಸಿಕೊಳ್ಳಿ - ಮೌಲ್ಯಗಳ ಉಲ್ಲೇಖಗಳು ಆವರಿಸಿತ್ತು ಮತ್ತು ಅಲ್ಪವಿರಾಮ ( "ಮೌಲ್ಯ 1", "VALUE2" ಇತ್ಯಾದಿ) ಬೇರ್ಪಡಿಸಲಾಗಿರುತ್ತದೆ ಇರಬೇಕು.';
+$lang['account_import_unable_to_determine_running_state'] = 'ಆಮದು ಚಾಲನೆಯಲ್ಲಿರುವ ನಿರ್ಧರಿಸಲು ಸಾಧ್ಯವಿಲ್ಲ.';
+$lang['account_import_upload_csv_file'] = 'CSV ಫೈಲ್ ಅಪ್ಲೋಡ್';
