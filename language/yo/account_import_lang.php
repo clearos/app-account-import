a1a5d85764cd8ac0516d0928aa278d2af39adbbc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:56:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Awọn Account wole app ṣe olopobobo wọle ti àpamọ lilo ohun Àwọn faili ni koma yà iye (CSV) kika.';
+$lang['account_import_app_name'] = 'Account wole';
+$lang['account_import_clear'] = 'Clear';
+$lang['account_import_csv_file'] = 'CSV Oluṣakoso';
+$lang['account_import_csv_file_not_found'] = 'CSV faili ko ba ri.';
+$lang['account_import_download_csv_template'] = 'Gba awọn CSV Àdàkọ';
+$lang['account_import_import_already_in_progress'] = 'Gbe wọle jẹ tẹlẹ ni ilọsiwaju.';
+$lang['account_import_import_complete'] = 'Gbe wọle pari.';
+$lang['account_import_import_log'] = 'gbe wọle Wọle';
+$lang['account_import_import_users'] = 'gbe wọle olumulo';
+$lang['account_import_importing_user'] = 'akowọle olumulo';
+$lang['account_import_no_data_found'] = 'Ko si data ri.';
+$lang['account_import_no_entries'] = 'Ko si wọle awọn titẹ sii ni won ri.';
+$lang['account_import_number_of_records'] = 'Nọmba ti Records';
+$lang['account_import_start_import'] = 'Bẹrẹ wole';
+$lang['account_import_tooltip_csv'] = 'Rii daju rẹ CSV faili ti wa ni akoonu daradara - o yẹ ki o ni iye paade ni ọrọ ati ki o niya nipa kan koma ( "value1", "value2" bbl).';
+$lang['account_import_unable_to_determine_running_state'] = 'Lagbara lati mọ ti o ba gbe wọle ti wa ni nṣiṣẹ.';
+$lang['account_import_upload_csv_file'] = 'Po si CSV Oluṣakoso';
