289bf91e7bb28ca4243340bf1d9f8ce9468baeb0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:40:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Приложение Account Импорт осуществляет массовый импорт счетов с использованием загруженного файла в формате значения, разделенные запятыми (CSV).';
+$lang['account_import_app_name'] = 'Импорт учетной записи';
+$lang['account_import_clear'] = 'Очистить';
+$lang['account_import_csv_file'] = 'CSV-файл';
+$lang['account_import_csv_file_not_found'] = 'CSV файл не найден.';
+$lang['account_import_download_csv_template'] = 'Загрузить CSV-шаблон';
+$lang['account_import_import_already_in_progress'] = 'Импорт уже идет.';
+$lang['account_import_import_complete'] = 'Импорт завершен.';
+$lang['account_import_import_log'] = 'Импорт Вход';
+$lang['account_import_import_users'] = 'Импорт пользователей';
+$lang['account_import_importing_user'] = 'Импорт пользователей';
+$lang['account_import_no_data_found'] = 'Данные не найдены.';
+$lang['account_import_no_entries'] = 'Не найдено ни одного импорта записей.';
+$lang['account_import_number_of_records'] = 'Количество записей';
+$lang['account_import_start_import'] = 'Start Import';
+$lang['account_import_tooltip_csv'] = 'Убедитесь, что ваш файл CSV отформатирован должным образом - оно должно иметь значения, заключенные в кавычки и разделенных запятой ( "value1", "value2" и т.д.).';
+$lang['account_import_unable_to_determine_running_state'] = 'Невозможно определить, если импорт работает.';
+$lang['account_import_upload_csv_file'] = 'Загрузить файл CSV';
