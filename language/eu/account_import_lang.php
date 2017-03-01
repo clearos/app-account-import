07d1f0a365364ce9af2ae74e24ce325d7b080a27
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:31:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Kontu-Import Aplikazioa ontziratu kontuak igotako komaz bereizitako balioak (CSV) formatuan fitxategi bat erabiliz import burutzen.';
+$lang['account_import_app_name'] = 'Kontu-Import';
+$lang['account_import_clear'] = 'Eguraldi';
+$lang['account_import_csv_file'] = 'CSV fitxategi';
+$lang['account_import_csv_file_not_found'] = 'CSV fitxategia ez da aurkitu.';
+$lang['account_import_download_csv_template'] = 'Deskargatu CSV Txantiloi';
+$lang['account_import_import_already_in_progress'] = 'Inportatu abian da dagoeneko.';
+$lang['account_import_import_complete'] = 'Inportazio osoa.';
+$lang['account_import_import_log'] = 'Inportazio erregistroa';
+$lang['account_import_import_users'] = 'Inportatu erabiltzaileak';
+$lang['account_import_importing_user'] = 'inportatzen erabiltzaile';
+$lang['account_import_no_data_found'] = 'Ez da daturik aurkitu.';
+$lang['account_import_no_entries'] = 'Ez da inportazio sarrerarik aurkitu.';
+$lang['account_import_number_of_records'] = 'Records kopurua';
+$lang['account_import_start_import'] = 'Inportatzen hasi';
+$lang['account_import_tooltip_csv'] = 'Ziurtatu zure CSV fitxategia formatu egokia da - aipuak artean daudela eta koma ( "value1", "balioa2" eta abar) arabera bereizitako balioak izan behar da.';
+$lang['account_import_unable_to_determine_running_state'] = 'Ezin da inportazio exekutatzen ari den zehazteko.';
+$lang['account_import_upload_csv_file'] = 'Igo CSV fitxategi';
