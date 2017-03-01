4d3da6172033623931c4ad91a6c658d5fe0a783f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:43:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'ගිණුම් ආනයන යෙදුම කොමාවලින් වෙන්කරන ලද අගයන් (CSV) ආකෘතියෙන් දී උඩුගත කල ගොනුව භාවිතා ගිණුම් තොග ආනයනය සිදු කරයි.';
+$lang['account_import_app_name'] = 'ගිණුම ආනයන';
+$lang['account_import_clear'] = 'පැහැදිලිව';
+$lang['account_import_csv_file'] = 'CSV ගොනුව';
+$lang['account_import_csv_file_not_found'] = 'CSV ගොනුවක් සොයාගත නොහැකි විය.';
+$lang['account_import_download_csv_template'] = 'CSV සැකිල්ල බාගත';
+$lang['account_import_import_already_in_progress'] = 'ආනයන දැනටමත් ක්රියාත්මක වේ.';
+$lang['account_import_import_complete'] = 'සම්පූර්ණ ආයාත කරන්න.';
+$lang['account_import_import_log'] = 'ආනයන ලොග්';
+$lang['account_import_import_users'] = 'ආනයන පරිශීලකයන්';
+$lang['account_import_importing_user'] = 'ආනයනය පරිශීලක';
+$lang['account_import_no_data_found'] = 'දත්ත නොමැත.';
+$lang['account_import_no_entries'] = 'ආනයන සටහන් ඇතුළත් කිරීම් හමුනොවිණි.';
+$lang['account_import_number_of_records'] = 'වාර්තා සංඛ්යාව';
+$lang['account_import_start_import'] = 'ආරම්භ ආනයන';
+$lang['account_import_tooltip_csv'] = 'ඔබගේ CSV ගොනුව නිසි ආකෘතිකරණය සහතික - එය වටිනාකම් උපුටා දැක්වීම් බහා හා කොමා ( "value1", "value2" ආදිය) විසින් වෙන් කර තිබිය යුතුය.';
+$lang['account_import_unable_to_determine_running_state'] = 'ආනයන ක්රියාත්මක වේ නම් නිර්ණය කිරීමට නොහැකි විය.';
+$lang['account_import_upload_csv_file'] = 'CSV ගොනුව උඩුගත කරන්න';
