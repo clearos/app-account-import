bd7d7d6d18ab1cfafa0819ce533c14cd2faa29d9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:20:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Sąskaitos Importas programa atlieka birių importo sąskaitas, naudojant įkeltas failas formatu kableliais atskirtų reikšmių (CSV).';
+$lang['account_import_app_name'] = 'sąskaita Importas';
+$lang['account_import_clear'] = 'aiškus';
+$lang['account_import_csv_file'] = 'CSV failo';
+$lang['account_import_csv_file_not_found'] = 'CSV failas nerastas.';
+$lang['account_import_download_csv_template'] = 'Atsisiųsti CSV šabloną';
+$lang['account_import_import_already_in_progress'] = 'Importas jau vyksta.';
+$lang['account_import_import_complete'] = 'Importas užbaigtas.';
+$lang['account_import_import_log'] = 'importas Prisijungti';
+$lang['account_import_import_users'] = 'importo Vartotojai';
+$lang['account_import_importing_user'] = 'importuoti vartotojas';
+$lang['account_import_no_data_found'] = 'Nėra duomenų.';
+$lang['account_import_no_entries'] = 'Importo įrašai nerasta.';
+$lang['account_import_number_of_records'] = 'Taškų Records';
+$lang['account_import_start_import'] = 'Pradėti Importas';
+$lang['account_import_tooltip_csv'] = 'Užtikrinkite, kad jūsų CSV failas tinkamai suformatuotas - ji turėtų turėti vertybes įdėtos citatos ir atskirtų kableliais ( "VALUE1", "VALUE2" ir tt).';
+$lang['account_import_unable_to_determine_running_state'] = 'Neįmanoma nustatyti, jeigu importas veikia.';
+$lang['account_import_upload_csv_file'] = 'Įkelti CSV failo';
