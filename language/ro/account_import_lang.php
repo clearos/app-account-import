2f34f109019dfc3460e295f0963698e92ee49b2a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:39:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Aplicația de cont Import efectuează importul în vrac de conturi folosind un fișier încărcat în format valori separate prin virgulă (CSV).';
+$lang['account_import_app_name'] = 'Import cont';
+$lang['account_import_clear'] = 'clar';
+$lang['account_import_csv_file'] = 'Fișier CSV';
+$lang['account_import_csv_file_not_found'] = 'fișier CSV nu a fost găsit.';
+$lang['account_import_download_csv_template'] = 'Descărcați CSV șablon';
+$lang['account_import_import_already_in_progress'] = 'Import este deja în curs de desfășurare.';
+$lang['account_import_import_complete'] = 'Import complet.';
+$lang['account_import_import_log'] = 'import Jurnal';
+$lang['account_import_import_users'] = 'Utilizatorii de import';
+$lang['account_import_importing_user'] = 'utilizator importă';
+$lang['account_import_no_data_found'] = 'Nu s-au gasit date.';
+$lang['account_import_no_entries'] = 's-au găsit intrările de import.';
+$lang['account_import_number_of_records'] = 'Numărul de înregistrări';
+$lang['account_import_start_import'] = 'Import de pornire';
+$lang['account_import_tooltip_csv'] = 'Asigurați-vă că fișierul CSV este corect formatat - ar trebui să aibă valori cuprinse in citate și separate printr-o virgulă ( "valoare1", "valoare2" etc.).';
+$lang['account_import_unable_to_determine_running_state'] = 'Nu se poate determina dacă importul se execută.';
+$lang['account_import_upload_csv_file'] = 'Încărcați fișierul CSV';
