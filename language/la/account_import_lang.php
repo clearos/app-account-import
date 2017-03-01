b0d6743d6e8e18f1833f8c4b4bfafae3961c26c7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:17:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'A app Account Import mole praestat file uploaded in comma pertinet ratione uti ad separatas bonis (CSV) format.';
+$lang['account_import_app_name'] = 'propter Import';
+$lang['account_import_clear'] = 'Aequaliter Nubila';
+$lang['account_import_csv_file'] = 'CSV lima';
+$lang['account_import_csv_file_not_found'] = 'CSV lima non inveni.';
+$lang['account_import_download_csv_template'] = 'Download CSV Template';
+$lang['account_import_import_already_in_progress'] = 'Summa est, iam in progressionis.';
+$lang['account_import_import_complete'] = 'Import perfecta.';
+$lang['account_import_import_log'] = 'Acta emendationum circumitarum';
+$lang['account_import_import_users'] = 'Import Users';
+$lang['account_import_importing_user'] = 'User importing';
+$lang['account_import_no_data_found'] = 'Data non invenit.';
+$lang['account_import_no_entries'] = 'Import entries non sunt inventi.';
+$lang['account_import_number_of_records'] = 'Number of Records';
+$lang['account_import_start_import'] = 'satus Import';
+$lang['account_import_tooltip_csv'] = 'Perficite CSV lima tua est recte formatae - inclusum habeat bona, ac per comma citationibus ( "value1" "value2" etc.).';
+$lang['account_import_unable_to_determine_running_state'] = 'Summa est, si non ad determinare currit.';
+$lang['account_import_upload_csv_file'] = 'CSV lima upload';
