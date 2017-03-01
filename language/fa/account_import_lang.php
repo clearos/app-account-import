df7cb4c725de995ea774139f817fc209e8760dac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:36:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'برنامه حساب واردات انجام واردات فله حساب با استفاده از یک فایل آپلود در ارزش کاما از هم جدا (CSV) فرمت.';
+$lang['account_import_app_name'] = 'واردات حساب';
+$lang['account_import_clear'] = 'واضح';
+$lang['account_import_csv_file'] = 'فایل CSV';
+$lang['account_import_csv_file_not_found'] = 'فایل CSV یافت نشد.';
+$lang['account_import_download_csv_template'] = 'دانلود الگوی CSV';
+$lang['account_import_import_already_in_progress'] = 'واردات در حال انجام است.';
+$lang['account_import_import_complete'] = 'واردات کامل است.';
+$lang['account_import_import_log'] = 'ورود';
+$lang['account_import_import_users'] = 'وارد کردن کاربران';
+$lang['account_import_importing_user'] = 'کاربران واردات';
+$lang['account_import_no_data_found'] = 'داده ای یافت نشد.';
+$lang['account_import_no_entries'] = 'هیچ نوشته ای واردات پیدا شد.';
+$lang['account_import_number_of_records'] = 'تعداد رکوردهای';
+$lang['account_import_start_import'] = 'شروع وارد';
+$lang['account_import_tooltip_csv'] = 'اطمینان از فایل CSV خود را به درستی فرمت شده - آن را باید ارزش محصور در نقل قولها و توسط یک کاما ( "VALUE1"، "VALUE2" و غیره) جدا شده اند.';
+$lang['account_import_unable_to_determine_running_state'] = 'برای تعیین اینکه آیا واردات در حال اجرا است ممکن نیست.';
+$lang['account_import_upload_csv_file'] = 'آپلود فایل CSV';
