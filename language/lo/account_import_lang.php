34022f2963638e627ea277467e1ad20dc20537a6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:19:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'ການ app ບັນຊີນໍາເຂົ້າປະຕິບັດການນໍາເຂົ້າຫຼາຍຂອງບັນຊີການນໍາໃຊ້ໄຟລ໌ທີ່ອັບໂຫລດໃນຈຸດແຍກຄ່າຮູບແບບ (CSV).';
+$lang['account_import_app_name'] = 'ນໍາເຂົ້າບັນຊີ';
+$lang['account_import_clear'] = 'ຈະແຈ້ງ';
+$lang['account_import_csv_file'] = 'File CSV';
+$lang['account_import_csv_file_not_found'] = 'ໄຟລ໌ CSV ບໍ່ໄດ້ພົບເຫັນ.';
+$lang['account_import_download_csv_template'] = 'ດາວໂຫລດ Template CSV';
+$lang['account_import_import_already_in_progress'] = 'ການນໍາເຂົ້າທີ່ມີຢູ່ແລ້ວໃນຄວາມຄືບຫນ້າ.';
+$lang['account_import_import_complete'] = 'ການນໍາເຂົ້າສະບັບສົມບູນ.';
+$lang['account_import_import_log'] = 'ການນໍາເຂົ້າເຂົ້າສູ່ລະບົບ';
+$lang['account_import_import_users'] = 'ຜູ້ໃຊ້ການນໍາເຂົ້າ';
+$lang['account_import_importing_user'] = 'ຜູ້ໃຊ້ການນໍາເຂົ້າ';
+$lang['account_import_no_data_found'] = 'ບໍ່ມີຂໍ້ມູນ.';
+$lang['account_import_no_entries'] = 'ບໍ່ມີການອອກສຽງນໍາເຂົ້າໄດ້ຖືກພົບເຫັນ.';
+$lang['account_import_number_of_records'] = 'ຈໍານວນການບັນທຶກ';
+$lang['account_import_start_import'] = 'ການເລີ່ມຕົ້ນນໍາເຂົ້າ';
+$lang['account_import_tooltip_csv'] = 'ຮັບປະກັນເອກະສານ CSV ຂອງທ່ານແມ່ນການຈັດຮູບແບບຢ່າງຖືກຕ້ອງ - ມັນຄວນຈະມີຄຸນຄ່າຂອງທີ່ຢູ່ໃນການຊື້ຂາຍແລະແຍກອອກໂດຍຈຸດ ( "value1", "value2" ແລະອື່ນໆ).';
+$lang['account_import_unable_to_determine_running_state'] = 'ບໍ່ສາມາດກໍານົດຖ້າຫາກວ່າການນໍາເຂົ້າເຮັດວຽກ.';
+$lang['account_import_upload_csv_file'] = 'ອັບໂຫຼດໄຟລ໌ CSV';
