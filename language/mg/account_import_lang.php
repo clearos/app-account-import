95809f5fde483e023a29a78b5c84e9d68a950afb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:22:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Ny Account Import fampiharana manao ampahany tena lanjan&#39;ny kaonty mampiasa ny rakitra ao amin&#39;ny faingo nakarina nisaraka soatoavina (CSV) endrika.';
+$lang['account_import_app_name'] = 'Account Import';
+$lang['account_import_clear'] = 'Clear';
+$lang['account_import_csv_file'] = 'CSV File';
+$lang['account_import_csv_file_not_found'] = 'CSV rakitra tsy hita.';
+$lang['account_import_download_csv_template'] = 'Download CSV Template';
+$lang['account_import_import_already_in_progress'] = 'Import dia efa ao amin&#39;ny fandrosoana.';
+$lang['account_import_import_complete'] = 'Import tanteraka.';
+$lang['account_import_import_log'] = 'Import Log';
+$lang['account_import_import_users'] = 'Import Users';
+$lang['account_import_importing_user'] = 'fanafarana mpampiasa';
+$lang['account_import_no_data_found'] = 'Tsy misy tahirin-kevitra hita.';
+$lang['account_import_no_entries'] = 'Tsy misy Import ampy hita.';
+$lang['account_import_number_of_records'] = 'Isan&#39;ny Records';
+$lang['account_import_start_import'] = 'Start Import';
+$lang['account_import_tooltip_csv'] = 'Antoka ny CSV rakitra dia format araka ny tokony ho - dia tokony hanana soatoavina fonosina ao anatin&#39;ny fakan-teny ary tafasaraka iray faingo ( "value1", "value2" sns).';
+$lang['account_import_unable_to_determine_running_state'] = 'Tsy afaka mamaritra raha Import no nihazakazaka.';
+$lang['account_import_upload_csv_file'] = 'Zavatra CSV File';
