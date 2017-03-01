9f49ab701e5a6676dea2a40a7675e39052cec220
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:23:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Te taupānga Pūkete Kawemai rave kawemai nuinga o ngā pūkete mā te whakamahi i te kōnae tukuake i roto i te piko wehea uara (CSV) hōputu.';
+$lang['account_import_app_name'] = 'Kawemai pūkete';
+$lang['account_import_clear'] = 'Ūkui';
+$lang['account_import_csv_file'] = 'Kōnae CSV';
+$lang['account_import_csv_file_not_found'] = 'kihai i kitea te kōnae CSV.';
+$lang['account_import_download_csv_template'] = 'Tikiake Tātauira CSV';
+$lang['account_import_import_already_in_progress'] = 'Ko kē Kawemai i te ahunga whakamua.';
+$lang['account_import_import_complete'] = 'Kawemai oti.';
+$lang['account_import_import_log'] = 'Kawemai Rangitaki';
+$lang['account_import_import_users'] = 'Kaiwhakamahi Kawemai';
+$lang['account_import_importing_user'] = 'kawemai kaiwhakamahi';
+$lang['account_import_no_data_found'] = 'kitea he raraunga.';
+$lang['account_import_no_entries'] = 'i kitea he tāurunga kawemai.';
+$lang['account_import_number_of_records'] = 'Tau o Records';
+$lang['account_import_start_import'] = 'Tīmata Kawemai';
+$lang['account_import_tooltip_csv'] = 'Whakarite whakahōpututia koutou kōnae CSV te tika - kia whai reira uara kopakia ana i roto i te mau faahitiraa parau, ka wehea e te piko ( "value1", "value2" etc).';
+$lang['account_import_unable_to_determine_running_state'] = 'Kāore e taea te whakatau, ki te te rere kawemai.';
+$lang['account_import_upload_csv_file'] = 'Tukuatu Kōnae CSV';
