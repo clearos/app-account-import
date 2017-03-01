650c8b2a1aadfe500cbbc5d4b25711717c94c74f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:34:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Konto Import app utfører bulk import av kontoer ved hjelp av en opplastet fil i kommadelte verdier (CSV) -format.';
+$lang['account_import_app_name'] = 'konto Import';
+$lang['account_import_clear'] = 'Klar';
+$lang['account_import_csv_file'] = 'CSV-fil';
+$lang['account_import_csv_file_not_found'] = 'CSV-fil ikke funnet.';
+$lang['account_import_download_csv_template'] = 'Last ned CSV-mal';
+$lang['account_import_import_already_in_progress'] = 'Import er allerede i gang.';
+$lang['account_import_import_complete'] = 'Import fullført.';
+$lang['account_import_import_log'] = 'import Log';
+$lang['account_import_import_users'] = 'Importer brukere';
+$lang['account_import_importing_user'] = 'Importere bruker';
+$lang['account_import_no_data_found'] = 'Ingen data funnet.';
+$lang['account_import_no_entries'] = 'Ingen importere oppføringer ble funnet.';
+$lang['account_import_number_of_records'] = 'Antall Records';
+$lang['account_import_start_import'] = 'start import';
+$lang['account_import_tooltip_csv'] = 'Sørg for CSV-filen er formatert riktig - det bør ha verdier vedlagt i anførselstegn og atskilt med komma ( "verdi1", "verdi2" osv).';
+$lang['account_import_unable_to_determine_running_state'] = 'Kan ikke fastslå om import kjører.';
+$lang['account_import_upload_csv_file'] = 'Last opp CSV-fil';
