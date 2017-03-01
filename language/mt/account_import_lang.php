8c6bfb8f10dbed98f15b03838b7660f1ba81ba86
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:30:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Il-app Kont Importazzjoni jwettaq importazzjoni bl-ingrossa ta &#39;kontijiet li jużaw fajl uploaded fil-valuri virgola separati (CSV)-format.';
+$lang['account_import_app_name'] = 'Importazzjoni kont';
+$lang['account_import_clear'] = 'ċar';
+$lang['account_import_csv_file'] = 'CSV File';
+$lang['account_import_csv_file_not_found'] = 'fajl CSV ma nstabx.';
+$lang['account_import_download_csv_template'] = 'Niżżel CSV Template';
+$lang['account_import_import_already_in_progress'] = 'Importazzjoni diġa jkun iseħħ.';
+$lang['account_import_import_complete'] = 'Importazzjoni kompluta.';
+$lang['account_import_import_log'] = 'importazzjoni Log';
+$lang['account_import_import_users'] = 'Utenti ta &#39;importazzjoni';
+$lang['account_import_importing_user'] = 'jimporta utent';
+$lang['account_import_no_data_found'] = 'M&#39;hemmx data misjuba.';
+$lang['account_import_no_entries'] = 'L-ebda annotament importazzjoni nstabu.';
+$lang['account_import_number_of_records'] = 'Għadd ta &#39;Rekords';
+$lang['account_import_start_import'] = 'bidu Importazzjoni';
+$lang['account_import_tooltip_csv'] = 'Tiżgura fajl CSV tiegħek huwa suppost ifformattjat - għandu jkollhom valuri magħluqa fit kwotazzjonijiet u separati b&#39;virgola ( "value1", "value2" eċċ).';
+$lang['account_import_unable_to_determine_running_state'] = 'Setgħux jiddeterminaw jekk l-importazzjoni tkun qed taħdem.';
+$lang['account_import_upload_csv_file'] = 'Upload CSV File';
