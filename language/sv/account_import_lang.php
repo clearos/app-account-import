8ad7ed821f24e332dd811734b90374cc40b1514e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:52:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Konto Import appen utför bulk import av konton med en uppladdad fil i kommaseparerade värden (CSV) format.';
+$lang['account_import_app_name'] = 'konto Import';
+$lang['account_import_clear'] = 'Klar';
+$lang['account_import_csv_file'] = 'CSV-fil';
+$lang['account_import_csv_file_not_found'] = 'CSV-fil hittades inte.';
+$lang['account_import_download_csv_template'] = 'Hämta CSV mall';
+$lang['account_import_import_already_in_progress'] = 'Import är redan pågår.';
+$lang['account_import_import_complete'] = 'Import komplett.';
+$lang['account_import_import_log'] = 'import Log';
+$lang['account_import_import_users'] = 'Importera användare';
+$lang['account_import_importing_user'] = 'importera användarkonton';
+$lang['account_import_no_data_found'] = 'Ingen information hittad.';
+$lang['account_import_no_entries'] = 'Inga import poster hittades.';
+$lang['account_import_number_of_records'] = 'Antal poster';
+$lang['account_import_start_import'] = 'Start Import';
+$lang['account_import_tooltip_csv'] = 'Se till att din CSV-fil är formaterad på rätt sätt - det bör ha värden inneslutna i citat och åtskilda av ett kommatecken ( "värde1", "värde2" etc.).';
+$lang['account_import_unable_to_determine_running_state'] = 'Det går inte att avgöra om import körs.';
+$lang['account_import_upload_csv_file'] = 'Överför CSV-fil';
