ebfc85ecb1e5f97702dd66eb81ee73ea9a025bfc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:55:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Ang Account Import app gumaganap bulk import ng mga account gamit ang isang nai-upload na file sa comma separated values ​​(CSV) na format.';
+$lang['account_import_app_name'] = 'Import account';
+$lang['account_import_clear'] = 'malinaw';
+$lang['account_import_csv_file'] = 'CSV File';
+$lang['account_import_csv_file_not_found'] = 'hindi natagpuan CSV file.';
+$lang['account_import_download_csv_template'] = 'I-download ang CSV Template';
+$lang['account_import_import_already_in_progress'] = 'Import ay nasa progreso.';
+$lang['account_import_import_complete'] = 'Import kumpleto.';
+$lang['account_import_import_log'] = 'Import Log';
+$lang['account_import_import_users'] = 'Import Users';
+$lang['account_import_importing_user'] = 'Pag-import ng user';
+$lang['account_import_no_data_found'] = 'Walang Nakitang data.';
+$lang['account_import_no_entries'] = 'No import entries ang natagpuan.';
+$lang['account_import_number_of_records'] = 'Bilang ng mga Records';
+$lang['account_import_start_import'] = 'Simulan ang Import';
+$lang['account_import_tooltip_csv'] = 'Tiyakin na ang iyong CSV file ay naka-format nang maayos - ito ay dapat magkaroon ng mga halaga na nakapaloob sa mga panipi at pinaghihiwalay ng kuwit ( "value1", "value2" atbp).';
+$lang['account_import_unable_to_determine_running_state'] = 'Hindi magawang matukoy kung import ay tumatakbo.';
+$lang['account_import_upload_csv_file'] = 'Mag-upload ng CSV File';
