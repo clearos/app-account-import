07b15dd0223fbbbaac1d8dcb98a81c3fb981385b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:38:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'အဆိုပါအကောင့်သွင်းကုန် app ကိုကော်မာကွဲကွာတန်ဖိုးများ (CSV) format နဲ့တစ်ဦးတင်ထားသောဖိုင်သုံးပြီးအကောင့်အသစ်များ၏အမြောက်အများတင်သွင်းသလေ့ရှိပါတယ်။';
+$lang['account_import_app_name'] = 'အကောင့်သွင်းကုန်';
+$lang['account_import_clear'] = 'ရှင်းလင်းသော';
+$lang['account_import_csv_file'] = 'CSV ဖိုင်ဖိုင်';
+$lang['account_import_csv_file_not_found'] = 'CSV ဖိုင်ရှာမတွေ့ပါ။';
+$lang['account_import_download_csv_template'] = 'CSV ဖိုင် Template ကို Download လုပ်';
+$lang['account_import_import_already_in_progress'] = 'သွင်းကုန်တိုးတက်မှုအတွက်ပြီးသားဖြစ်ပါတယ်။';
+$lang['account_import_import_complete'] = 'ပြီးပြည့်စုံတင်သွင်းပါ။';
+$lang['account_import_import_log'] = 'သွင်းကုန်ပါ';
+$lang['account_import_import_users'] = 'သွင်းကုန်အသုံးပြုသူများ';
+$lang['account_import_importing_user'] = 'အသုံးပြုသူတင်သွင်းခြင်း';
+$lang['account_import_no_data_found'] = 'ဒေတာမတွေ့ပါ။';
+$lang['account_import_no_entries'] = 'သွင်းကုန် entries တွေကိုတွေ့ရှိခဲ့သည်မရှိပါ။';
+$lang['account_import_number_of_records'] = 'Records ကိုအရေအတွက်';
+$lang['account_import_start_import'] = 'သွင်းကုန် Start';
+$lang['account_import_tooltip_csv'] = 'သင့်ရဲ့ CSV ဖိုင်စနစ်တကျချပ်သည်အာမခံ - ကတန်ဖိုးများကိုကိုးကားအတွက်ပူးတွဲနှင့်တစ်ဦးကော်မာ ( "VALUE1", "VALUE2" စတာတွေ) ကကွဲကွာခဲ့ကြသင့်သည်။';
+$lang['account_import_unable_to_determine_running_state'] = 'သွင်းကုန်ပြေးလျှင်ဆုံးဖြတ်ရန်မဖြစ်နိုင်ပါ။';
+$lang['account_import_upload_csv_file'] = 'CSV ဖိုင်ဖိုင် Upload လုပ်ပါ';
