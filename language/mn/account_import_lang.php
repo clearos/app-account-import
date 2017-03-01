61c90c2f9f292b0a7cbf779d5cff32b008876faa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:27:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Дансны Импортын апп таслалаар тусгаарлаж утга (CSV) хэлбэрээр байршуулсан файл ашиглан дансны ихэнх нь импорт хийдэг.';
+$lang['account_import_app_name'] = 'Дансны Импортын';
+$lang['account_import_clear'] = 'Clear';
+$lang['account_import_csv_file'] = 'CSV Файлын';
+$lang['account_import_csv_file_not_found'] = 'CSV файл олдсонгүй.';
+$lang['account_import_download_csv_template'] = 'CSV Загвар татаж авах';
+$lang['account_import_import_already_in_progress'] = 'Импортын ахиц дэвшил аль хэдийн байна.';
+$lang['account_import_import_complete'] = 'Импорт нь бүрэн.';
+$lang['account_import_import_log'] = 'Импортын нэвтрэх';
+$lang['account_import_import_users'] = 'Импортын гишүүд';
+$lang['account_import_importing_user'] = 'импортлогч хэрэглэгч';
+$lang['account_import_no_data_found'] = 'Ямар ч мэдээлэл олдлоо.';
+$lang['account_import_no_entries'] = 'Ямар ч импортын оруулгууд нь олсон байна.';
+$lang['account_import_number_of_records'] = 'Records тоо';
+$lang['account_import_start_import'] = 'Start Импортын';
+$lang['account_import_tooltip_csv'] = 'Таны CSV файл нь зөв форматалсан хангах - энэ утгуудыг Үнийн нь хаалттай бөгөөд таслалаар ( "value1", "VALUE2" гэх мэт) тусгаарлагдсан байх ёстой.';
+$lang['account_import_unable_to_determine_running_state'] = 'импортын ажиллаж байгаа эсэхийг тодорхойлохын тулд боломжгүй.';
+$lang['account_import_upload_csv_file'] = 'CSV файлыг upload хийнэ';
