15eb1dd3b5e1322479a6eec70cade59352348dda
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:26:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'അക്കൗണ്ട് ഇംപോർട്ട് അപ്ലിക്കേഷൻ കോമാ വേർതിരിച്ച മൂല്യങ്ങൾ (CSV) ഫോർമാറ്റിൽ അപ്ലോഡ് ഫയൽ ഉപയോഗിച്ച് അക്കൗണ്ടുകൾ ബൾക്ക് ഇംപോർട്ട് നടത്തുന്നു.';
+$lang['account_import_app_name'] = 'അക്കൗണ്ട് ഇംപോർട്ട്';
+$lang['account_import_clear'] = 'തെളിഞ്ഞ';
+$lang['account_import_csv_file'] = 'CSV ഫയൽ';
+$lang['account_import_csv_file_not_found'] = 'CSV ഫയൽ കണ്ടെത്തിയില്ല.';
+$lang['account_import_download_csv_template'] = 'CSV- ൽ ടെംപ്ലേറ്റ് ഡൌൺലോഡ്';
+$lang['account_import_import_already_in_progress'] = 'ഇംപോർട്ട് ഇതിനകം പുരോഗതിയിലാണ്.';
+$lang['account_import_import_complete'] = 'ഇറക്കുമതി പൂർത്തിയായി.';
+$lang['account_import_import_log'] = 'ഇംപോർട്ട് ലോഗ്';
+$lang['account_import_import_users'] = 'ഇംപോർട്ട് ഉപയോക്താക്കൾ';
+$lang['account_import_importing_user'] = 'ഉപയോക്താവ് ഇറക്കുമതി';
+$lang['account_import_no_data_found'] = 'ഡാറ്റകളൊന്നും കണ്ടെത്തിയില്ല.';
+$lang['account_import_no_entries'] = 'ഇറക്കുമതി എൻട്രികൾ കണ്ടെത്തിയില്ല.';
+$lang['account_import_number_of_records'] = 'റിക്കോർഡ്സ് എണ്ണം';
+$lang['account_import_start_import'] = 'ഇറക്കുമതി ആരംഭിക്കുക';
+$lang['account_import_tooltip_csv'] = 'നിങ്ങളുടെ CSV ഫയൽ ശരിയായി ഫോർമാറ്റ് ഉറപ്പാക്കുക - അതു മൂല്യങ്ങൾ ഉദ്ധരണികൾ വരുന്ന പദങ്ങളുടെ ഒരു കോമ ( "VALUE1", "VALUE2" മുതലായവ) വേർതിരിച്ച ചെയ്യണം.';
+$lang['account_import_unable_to_determine_running_state'] = 'ഇമ്പോർട്ട് പ്രവർത്തിക്കുന്ന നിർണയിക്കാൻ കഴിയുന്നില്ല.';
+$lang['account_import_upload_csv_file'] = 'CSV ഫയൽ അപ്ലോഡ്';
