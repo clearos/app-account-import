01b2fbb18ec655f0a4af50bcddf8be08879f1722
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:42:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = '该帐户导入应用程序执行使用逗号分隔值（CSV）格式上传文件账户批量导入。';
+$lang['account_import_app_name'] = '帐户导入';
+$lang['account_import_clear'] = '明确';
+$lang['account_import_csv_file'] = 'CSV文件';
+$lang['account_import_csv_file_not_found'] = 'CSV文件未找到。';
+$lang['account_import_download_csv_template'] = '下载CSV模板';
+$lang['account_import_import_already_in_progress'] = '进口已在进行中。';
+$lang['account_import_import_complete'] = '导入完成。';
+$lang['account_import_import_log'] = '导入日志';
+$lang['account_import_import_users'] = '导入用户';
+$lang['account_import_importing_user'] = '导入用户';
+$lang['account_import_no_data_found'] = '没有找到数据。';
+$lang['account_import_no_entries'] = '未发现进口的条目。';
+$lang['account_import_number_of_records'] = '记录的数';
+$lang['account_import_start_import'] = '开始导入';
+$lang['account_import_tooltip_csv'] = '确保您的CSV文件格式正确 - 它应该包含在报价和用逗号（“值1”，“值2”等）分隔值。';
+$lang['account_import_unable_to_determine_running_state'] = '无法确定是否导入正在运行。';
+$lang['account_import_upload_csv_file'] = '上传CSV文件';
