df9ebad9da5abd46d0d44691369923d285fc370a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:23:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Die rekening invoer app voer grootmaat invoer van rekeninge met behulp van &#39;n opgelaaide lêer in kommas geskei waardes (CSV) formaat.';
+$lang['account_import_app_name'] = 'rekening invoer';
+$lang['account_import_clear'] = 'duidelik';
+$lang['account_import_csv_file'] = 'CSV';
+$lang['account_import_csv_file_not_found'] = 'CSV nie gevind nie.';
+$lang['account_import_download_csv_template'] = 'Laai CSV Sjabloon';
+$lang['account_import_import_already_in_progress'] = 'Voer is reeds aan die gang.';
+$lang['account_import_import_complete'] = 'Invoer voltooi.';
+$lang['account_import_import_log'] = 'invoer Meld';
+$lang['account_import_import_users'] = 'invoer Gebruikers';
+$lang['account_import_importing_user'] = 'Die invoer van gebruiker';
+$lang['account_import_no_data_found'] = 'Geen data gevind.';
+$lang['account_import_no_entries'] = 'Geen inskrywings invoer gevind.';
+$lang['account_import_number_of_records'] = 'Aantal rekords';
+$lang['account_import_start_import'] = 'Begin Invoer';
+$lang['account_import_tooltip_csv'] = 'Maak seker dat jou CSV behoorlik geformateer - dit moet waardes ingesluit in aanhalings en geskei deur &#39;n komma ( "waarde1", "waarde2" ens) het.';
+$lang['account_import_unable_to_determine_running_state'] = 'Nie in staat om vas te stel of die invoer loop.';
+$lang['account_import_upload_csv_file'] = 'Laai CSV';
