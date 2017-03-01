32e87f0ae6d60fae53674fe30b9bb412314f653e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:28:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'खाते आयात अनुप्रयोग स्वल्पविरामाने विभक्त केलेली मूल्ये (सी) स्वरूपात अपलोड फाइल वापरून खाती मोठ्या प्रमाणात आयात करते.';
+$lang['account_import_app_name'] = 'खाते आयात';
+$lang['account_import_clear'] = 'साफ करा';
+$lang['account_import_csv_file'] = 'सी फाइल';
+$lang['account_import_csv_file_not_found'] = 'सी फाइल आढळली नाही.';
+$lang['account_import_download_csv_template'] = 'सी टेम्पलेट डाउनलोड करा';
+$lang['account_import_import_already_in_progress'] = 'आयात आधीपासूनच प्रगतीपथावर आहे.';
+$lang['account_import_import_complete'] = 'आयात पूर्ण.';
+$lang['account_import_import_log'] = 'आयात लॉग';
+$lang['account_import_import_users'] = 'वापरकर्ते आयात';
+$lang['account_import_importing_user'] = 'आयात वापरकर्ता';
+$lang['account_import_no_data_found'] = 'डेटा आढळला नाही.';
+$lang['account_import_no_entries'] = 'आयात नोंदी आढळून आले होते.';
+$lang['account_import_number_of_records'] = 'रेकॉर्ड संख्या';
+$lang['account_import_start_import'] = 'आयात प्रारंभ';
+$lang['account_import_tooltip_csv'] = 'आपल्या CSV फाइलमध्ये योग्यरितीने स्वरूपित आहे याची खात्री करा - तो मूल्यांचे आंतरशालेय मध्ये बंद आणि स्वल्पविरामाने विभक्त ( "मूल्य 1", "मूल्य 2" इ) करून असणे आवश्यक आहे.';
+$lang['account_import_unable_to_determine_running_state'] = 'आयात चालू आहे तर निर्धारीत करण्यात अक्षम.';
+$lang['account_import_upload_csv_file'] = 'सी फाइल अपलोड करा';
