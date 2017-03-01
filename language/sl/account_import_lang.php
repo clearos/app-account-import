ada78bcdc3f95ab159fbcc8f549b2ed537e9219a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:45:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Račun Import aplikacija izvaja v razsutem stanju uvoz računov z uporabo naloženo datoteko v vejico ločene vrednosti (CSV) formatu.';
+$lang['account_import_app_name'] = 'Uvozi';
+$lang['account_import_clear'] = 'Počisti';
+$lang['account_import_csv_file'] = 'CSV datoteke';
+$lang['account_import_csv_file_not_found'] = 'CSV datoteke ni bilo mogoče najti.';
+$lang['account_import_download_csv_template'] = 'Naložite CSV predloge';
+$lang['account_import_import_already_in_progress'] = 'Uvoz je že v teku.';
+$lang['account_import_import_complete'] = 'Uvoz je dokončan.';
+$lang['account_import_import_log'] = 'Uvoz Log';
+$lang['account_import_import_users'] = 'uvozna Uporabniki';
+$lang['account_import_importing_user'] = 'uvažanje uporabnik';
+$lang['account_import_no_data_found'] = 'Ni podatkov.';
+$lang['account_import_no_entries'] = 'Uvozne vnosi niso bili najdeni.';
+$lang['account_import_number_of_records'] = 'Število zapisov';
+$lang['account_import_start_import'] = 'Začetek Import';
+$lang['account_import_tooltip_csv'] = 'Preverite, ali je datoteka CSV je pravilno oblikovana - mora imeti vrednote zaprte v citatov in ločene z vejico ( "VALUE1", "VALUE2" itd).';
+$lang['account_import_unable_to_determine_running_state'] = 'Ni mogoče ugotoviti, kadar se uvoz teče.';
+$lang['account_import_upload_csv_file'] = 'Naloži datoteko CSV';
