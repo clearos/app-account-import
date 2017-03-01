6c2b30c8fae3d677fde723f7e6ba75e1c7500cda
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:33:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'De Account Import app voert bulk import van rekeningen met behulp van een geüpload bestand in komma&#39;s gescheiden waarden (CSV) formaat.';
+$lang['account_import_app_name'] = 'account Import';
+$lang['account_import_clear'] = 'Duidelijk';
+$lang['account_import_csv_file'] = 'CSV-bestand';
+$lang['account_import_csv_file_not_found'] = 'CSV-bestand niet gevonden.';
+$lang['account_import_download_csv_template'] = 'Download CSV Template';
+$lang['account_import_import_already_in_progress'] = 'Import is al aan de gang.';
+$lang['account_import_import_complete'] = 'Import voltooid.';
+$lang['account_import_import_log'] = 'import Log';
+$lang['account_import_import_users'] = 'Gebruikers importeren';
+$lang['account_import_importing_user'] = 'Importeren gebruiker';
+$lang['account_import_no_data_found'] = 'Geen data gevonden.';
+$lang['account_import_no_entries'] = 'Geen data import gevonden.';
+$lang['account_import_number_of_records'] = 'Aantal Records';
+$lang['account_import_start_import'] = 'importeren starten';
+$lang['account_import_tooltip_csv'] = 'Zorg ervoor dat uw CSV-bestand wordt correct opgemaakt - het moet waarden ingesloten in offertes en gescheiden door een komma ( "waarde1", "waarde2" etc.).';
+$lang['account_import_unable_to_determine_running_state'] = 'Niet in staat om te bepalen of de invoer wordt uitgevoerd.';
+$lang['account_import_upload_csv_file'] = 'Upload CSV-bestand';
