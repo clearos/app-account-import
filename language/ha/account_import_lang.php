d771ad7b9b10bd711c190abd875d5b305f534450
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:59:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'A Account Import app yi girma shigo da asusun yin amfani da wani uploaded fayil a wakafi rabu dabi&#39;u (CSV) format.';
+$lang['account_import_app_name'] = 'account Import';
+$lang['account_import_clear'] = 'Sunny';
+$lang['account_import_csv_file'] = 'CSV fayil';
+$lang['account_import_csv_file_not_found'] = 'CSV file ba su samu.';
+$lang['account_import_download_csv_template'] = 'Download CSV allo';
+$lang['account_import_import_already_in_progress'] = 'Import ne riga a ci gaba.';
+$lang['account_import_import_complete'] = 'Import complete.';
+$lang['account_import_import_log'] = 'Import Log';
+$lang['account_import_import_users'] = 'Import Users';
+$lang['account_import_importing_user'] = 'shigo mai amfani';
+$lang['account_import_no_data_found'] = 'No data samu.';
+$lang['account_import_no_entries'] = 'No shigo shigarwar da aka samu.';
+$lang['account_import_number_of_records'] = 'Number of Records';
+$lang['account_import_start_import'] = 'Fara Import';
+$lang['account_import_tooltip_csv'] = 'Tabbatar da your CSV fayil aka tsara yadda ya kamata - ya kamata da dabi&#39;u a kewaye a cikin ambato kuma rabu da wakafi ( "value1", "value2" da dai sauransu).';
+$lang['account_import_unable_to_determine_running_state'] = 'Kasa domin sanin idan shigo ne a guje.';
+$lang['account_import_upload_csv_file'] = 'Upload CSV fayil';
