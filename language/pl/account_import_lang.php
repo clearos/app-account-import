8d8101c593350f64a94ac4b3b100d36585dca62f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:37:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Aplikacja wykonuje Konto importu zbiorczego importu kont z wykorzystaniem przesłany plik w formacie wartości oddzielonych przecinkami (CSV).';
+$lang['account_import_app_name'] = 'Importuj konto';
+$lang['account_import_clear'] = 'Jasny';
+$lang['account_import_csv_file'] = 'Plik CSV';
+$lang['account_import_csv_file_not_found'] = 'Nie znaleziono pliku CSV.';
+$lang['account_import_download_csv_template'] = 'Pobierz szablon CSV';
+$lang['account_import_import_already_in_progress'] = 'Importu jest już w toku.';
+$lang['account_import_import_complete'] = 'Importowanie zakończone.';
+$lang['account_import_import_log'] = 'Log importu';
+$lang['account_import_import_users'] = 'Importowanie użytkowników';
+$lang['account_import_importing_user'] = 'Importowanie użytkownika';
+$lang['account_import_no_data_found'] = 'Nie znaleziono danych.';
+$lang['account_import_no_entries'] = 'Brak wpisów przywozowe zostały znalezione.';
+$lang['account_import_number_of_records'] = 'Liczba rekordów';
+$lang['account_import_start_import'] = 'Rozpocznij import';
+$lang['account_import_tooltip_csv'] = 'Upewnij się, że plik CSV jest prawidłowo sformatowany - powinien mieć wartości ujęty w notowaniach i oddzielonych przecinkiem ( "", "VALUE1 VALUE2" itd).';
+$lang['account_import_unable_to_determine_running_state'] = 'Nie udało się ustalić, czy import jest uruchomiony.';
+$lang['account_import_upload_csv_file'] = 'Prześlij plik CSV';
