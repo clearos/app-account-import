ab5635775c4e2c4d231c878e79ccbe85158f1471
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:55:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Account Import ilova vergul bilan ajratilgan qiymatlar (CSV) formatida bir yuklangan faylni yordamida hisob asosiy qismi import amalga oshiradi.';
+$lang['account_import_app_name'] = 'Account import';
+$lang['account_import_clear'] = 'aniq';
+$lang['account_import_csv_file'] = 'CSV Fayl';
+$lang['account_import_csv_file_not_found'] = 'CSV fayl topilmadi.';
+$lang['account_import_download_csv_template'] = 'CSV shablonni ko&#39;chirib';
+$lang['account_import_import_already_in_progress'] = 'Import taraqqiyot allaqachon.';
+$lang['account_import_import_complete'] = 'Import to&#39;liq.';
+$lang['account_import_import_log'] = 'import Kirish';
+$lang['account_import_import_users'] = 'import foydalanuvchilar';
+$lang['account_import_importing_user'] = 'import foydalanuvchi';
+$lang['account_import_no_data_found'] = 'Ma&#39;lumotlar topilmadi.';
+$lang['account_import_no_entries'] = 'No import yozuvlari topilgan.';
+$lang['account_import_number_of_records'] = 'Records soni';
+$lang['account_import_start_import'] = 'Start import';
+$lang['account_import_tooltip_csv'] = 'Sizning CSV fayl to&#39;g&#39;ri formatlangan ta&#39;minlash - bu qadriyatlar iborada ilova va vergul ( "VALUE1", "değer2" va boshqalar) bilan ajratilgan bo&#39;lishi kerak.';
+$lang['account_import_unable_to_determine_running_state'] = 'import ishlayotgan bo&#39;lsa aniqlash bo&#39;lmadi.';
+$lang['account_import_upload_csv_file'] = 'CSV File yuklash';
