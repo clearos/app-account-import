12a77d068986241c73bbbe4a610ea8640408127a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:52:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gl/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'A aplicación conta Import executa importación masiva de contas mediante un ficheiro cargado en valores separados por comas formato (CSV).';
+$lang['account_import_app_name'] = 'importación conta';
+$lang['account_import_clear'] = 'claro';
+$lang['account_import_csv_file'] = 'ficheiro CSV';
+$lang['account_import_csv_file_not_found'] = 'ficheiro CSV non se atopou.';
+$lang['account_import_download_csv_template'] = 'Descargar modelo CSV';
+$lang['account_import_import_already_in_progress'] = 'Importación xa está en marcha.';
+$lang['account_import_import_complete'] = 'Importación completa.';
+$lang['account_import_import_log'] = 'Import Log';
+$lang['account_import_import_users'] = 'descargar usuarios';
+$lang['account_import_importing_user'] = 'usuario importador';
+$lang['account_import_no_data_found'] = 'Non se encontraron datos.';
+$lang['account_import_no_entries'] = 'Ningunha entrada de importación foron atopados.';
+$lang['account_import_number_of_records'] = 'Número de rexistros';
+$lang['account_import_start_import'] = 'iniciar importación';
+$lang['account_import_tooltip_csv'] = 'Garantir o seu ficheiro CSV está formatado correctamente - debe ter valores entre comiñas e separados por unha coma ( "value1", "value2" etc.).';
+$lang['account_import_unable_to_determine_running_state'] = 'Non se puido determinar se a importación en execución.';
+$lang['account_import_upload_csv_file'] = 'Enviar ficheiro CSV';
