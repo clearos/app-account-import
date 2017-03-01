5251958df8c8cc0f88b2eb572438206f5ec2d461
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:58:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'App a Kont Import fè èstime enpòte nan kont lè l sèvi avèk yon dosye Uploaded nan vigil separe valè (CSV) fòma.';
+$lang['account_import_app_name'] = 'Kont Import';
+$lang['account_import_clear'] = 'Klè';
+$lang['account_import_csv_file'] = 'CSV File';
+$lang['account_import_csv_file_not_found'] = 'CSV dosye pa jwenn.';
+$lang['account_import_download_csv_template'] = 'Download CSV Modèl';
+$lang['account_import_import_already_in_progress'] = 'Import se deja nan pwogrè.';
+$lang['account_import_import_complete'] = 'Import konplè.';
+$lang['account_import_import_log'] = 'Import Log';
+$lang['account_import_import_users'] = 'Itilizatè yo Import';
+$lang['account_import_importing_user'] = 'enpòte itilizatè';
+$lang['account_import_no_data_found'] = 'Pa gen done te jwenn.';
+$lang['account_import_no_entries'] = 'Pa gen antre enpòte yo te jwenn.';
+$lang['account_import_number_of_records'] = 'Nimewo nan Albòm';
+$lang['account_import_start_import'] = 'Start Import';
+$lang['account_import_tooltip_csv'] = 'Asire se dosye CSV ou fòma byen - li ta dwe gen valè fèmen nan sityasyon ak separe pa yon vigil ( "value1", "value2" elatriye).';
+$lang['account_import_unable_to_determine_running_state'] = 'Kapab detèmine si enpòte ap kouri.';
+$lang['account_import_upload_csv_file'] = 'Voye CSV File';
