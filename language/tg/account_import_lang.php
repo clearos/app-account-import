520e878c6a16776bd4076f544e8fe8ed69acbde4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:54:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Дар барнома Ҳисоби Ворид иҷро воридоти Ќисми ҳисобҳои мавриди истифодаи файли боргузорӣ дар арзишҳои вергул ҷудо (CSV) формати.';
+$lang['account_import_app_name'] = 'Воридоти Ҳисоби';
+$lang['account_import_clear'] = 'Clear';
+$lang['account_import_csv_file'] = 'CSV File';
+$lang['account_import_csv_file_not_found'] = 'файл CSV ёфт нашуд.';
+$lang['account_import_download_csv_template'] = 'Download CSV Шаблон';
+$lang['account_import_import_already_in_progress'] = 'Воридоти аст, аллакай дар пешрафти.';
+$lang['account_import_import_complete'] = 'Воридоти пурра.';
+$lang['account_import_import_log'] = 'Воридоти Русский';
+$lang['account_import_import_users'] = 'Воридоти Истифодабарандагон';
+$lang['account_import_importing_user'] = 'истифодабаранда воридоти';
+$lang['account_import_no_data_found'] = 'Не маълумоти ёфт.';
+$lang['account_import_no_entries'] = 'Не вурудоти воридоти ёфт шуд.';
+$lang['account_import_number_of_records'] = 'Шумораи Records';
+$lang['account_import_start_import'] = 'Start Воридоти';
+$lang['account_import_tooltip_csv'] = 'Боварӣ файл CSV шумо дуруст, ки чунин формат - он бояд арзишҳои замима дар нархномањо ва вергул ( «value1», «value2» ва ғайра) ҷудо бошад.';
+$lang['account_import_unable_to_determine_running_state'] = 'муайян, агар воридоти иҷро мешавад Қодир.';
+$lang['account_import_upload_csv_file'] = 'Боргузорӣ CSV File';
