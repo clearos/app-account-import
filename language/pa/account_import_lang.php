ba8ffe94a0cac123bb594ba2298c05aef8b7a128
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:35:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'ਖਾਤਾ ਆਯਾਤ ਐਪਲੀਕੇਸ਼ ਨੂੰ ਕਾਮੇ ਨਾਲ ਵੱਖ ਮੁੱਲ (CSV) ਫਾਰਮੈਟ ਵਿੱਚ ਇੱਕ ਨੂੰ ਅੱਪਲੋਡ ਫਾਇਲ ਵਰਤ ਖਾਤੇ ਦੇ ਬਲਕ ਆਯਾਤ ਕਰਦਾ ਹੈ.';
+$lang['account_import_app_name'] = 'ਖਾਤਾ ਆਯਾਤ';
+$lang['account_import_clear'] = 'ਆਸਮਾਨ';
+$lang['account_import_csv_file'] = 'CSV ਫਾਇਲ';
+$lang['account_import_csv_file_not_found'] = 'CSV ਫਾਇਲ ਨਹੀ ਲੱਭੀ ਹੈ.';
+$lang['account_import_download_csv_template'] = 'ਡਾਊਨਲੋਡ CSV ਫਰਮਾ';
+$lang['account_import_import_already_in_progress'] = 'ਅਯਾਤ ਹੀ ਪ੍ਰਗਤੀ ਵਿੱਚ ਹੈ.';
+$lang['account_import_import_complete'] = 'ਇੰਪੋਰਟ ਮੁਕੰਮਲ ਹੋ.';
+$lang['account_import_import_log'] = 'ਆਯਾਤ ਲਾਗ';
+$lang['account_import_import_users'] = 'ਆਯਾਤ ਉਪਭੋਗੀ';
+$lang['account_import_importing_user'] = 'ਅਯਾਤ ਉਪਭੋਗੀ ਨੂੰ';
+$lang['account_import_no_data_found'] = 'ਕੋਈ ਡਾਟਾ ਮਿਲਿਆ ਹੈ.';
+$lang['account_import_no_entries'] = 'ਕੋਈ ਆਯਾਤ ਇੰਦਰਾਜ਼ ਪਾਇਆ ਗਿਆ ਸੀ.';
+$lang['account_import_number_of_records'] = 'ਰਿਕਾਰਡ ਦੀ ਗਿਣਤੀ';
+$lang['account_import_start_import'] = 'ਸ਼ੁਰੂ ਆਯਾਤ';
+$lang['account_import_tooltip_csv'] = 'ਯਕੀਨੀ ਆਪਣੇ CSV ਫਾਇਲ ਨੂੰ ਠੀਕ ਫਾਰਮੈਟ ਹੈ - ਇਸ ਨੂੰ ਮੁੱਲ ਹਵਾਲੇ ਵਿੱਚ ਨੱਥੀ ਹੈ ਅਤੇ ਇੱਕ ਕਾਮੇ ( "value1", "value2" ਆਦਿ) ਨਾਲ ਵੱਖ ਹੋਣਾ ਚਾਹੀਦਾ ਹੈ.';
+$lang['account_import_unable_to_determine_running_state'] = 'ਜੇ ਦਰਾਮਦ ਚੱਲ ਰਿਹਾ ਹੈ ਇਹ ਪਤਾ ਕਰਨ ਲਈ ਅਸਫ਼ਲ ਹੈ.';
+$lang['account_import_upload_csv_file'] = 'ਅੱਪਲੋਡ CSV ਫਾਇਲ';
