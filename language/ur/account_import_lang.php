f2462caab22ac8e8e6c3af5f4def9d7d0e5076a8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:55:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'اکاؤنٹ درآمد ایپ کو کوما سے علیحدہ اقدار (CSV) کی شکل میں ایک اپ لوڈ کردہ فائل کا استعمال کرتے ہوئے اکاؤنٹس کی بلک درآمد انجام دیتا.';
+$lang['account_import_app_name'] = 'اکاؤنٹ درآمد';
+$lang['account_import_clear'] = 'صاف';
+$lang['account_import_csv_file'] = 'CSV فائل';
+$lang['account_import_csv_file_not_found'] = 'CSV فائل نہیں ملی.';
+$lang['account_import_download_csv_template'] = 'لوڈ CSV سانچہ';
+$lang['account_import_import_already_in_progress'] = 'درآمد کے پہلے سے جاری ہے.';
+$lang['account_import_import_complete'] = 'درآمد کے مکمل.';
+$lang['account_import_import_log'] = 'درآمد کے تبا';
+$lang['account_import_import_users'] = 'درآمدی صارفین';
+$lang['account_import_importing_user'] = 'کی درآمد صارف';
+$lang['account_import_no_data_found'] = 'کوئی ڈیٹا نہیں ملا.';
+$lang['account_import_no_entries'] = 'کوئی درآمد کے اندراجات پائے گئے.';
+$lang['account_import_number_of_records'] = 'ریکارڈ کی تعداد';
+$lang['account_import_start_import'] = 'شروع درآمد';
+$lang['account_import_tooltip_csv'] = 'آپ CSV فائل مناسب طریقے سے فارمیٹ ہے کو یقینی بنانے - یہ اقدار کوٹیشن میں منسلک اور ایک کوما ( "VALUE1"، "VALUE2" وغیرہ) کی طرف سے الگ ہونا چاہئے.';
+$lang['account_import_unable_to_determine_running_state'] = 'اگر درآمدی چل رہا ہے کا تعین کرنے سے قاصر ہے.';
+$lang['account_import_upload_csv_file'] = 'CSV فائل اپ لوڈ کریں';
