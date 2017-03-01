186d8efae7e1b962d380b645ca800ef69f365776
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:55:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Mae&#39;r app Mewnforio Cyfrif perfformio mewnforio rhan fwyaf o gyfrifon yn defnyddio ffeil llwytho i fyny mewn gwerthoedd coma gwahanu fformat (CSV).';
+$lang['account_import_app_name'] = 'Mewnforio cyfrif';
+$lang['account_import_clear'] = 'Clear';
+$lang['account_import_csv_file'] = 'CSV Ffeil';
+$lang['account_import_csv_file_not_found'] = 'CSV file heb ei darganfod.';
+$lang['account_import_download_csv_template'] = 'Download CSV Templed';
+$lang['account_import_import_already_in_progress'] = 'Mewnforio eisoes ar y gweill.';
+$lang['account_import_import_complete'] = 'Mewnforio cyflawn.';
+$lang['account_import_import_log'] = 'mewnforio Log';
+$lang['account_import_import_users'] = 'Defnyddwyr mewnforio';
+$lang['account_import_importing_user'] = 'mewnforio defnyddiwr';
+$lang['account_import_no_data_found'] = 'Ni chafwyd data.';
+$lang['account_import_no_entries'] = 'Ni chanfuwyd unrhyw gofnodion mewnforio.';
+$lang['account_import_number_of_records'] = 'Nifer o Records';
+$lang['account_import_start_import'] = 'Mewnforio Start';
+$lang['account_import_tooltip_csv'] = 'Sicrhewch eich ffeil CSV yn cael ei fformatio yn iawn - dylai fod ganddynt werthoedd hamgáu mewn dyfyniadau a gwahanu gan goma ( "value1", "value2" ac ati).';
+$lang['account_import_unable_to_determine_running_state'] = 'Methu penderfynu os mewnforio yn rhedeg.';
+$lang['account_import_upload_csv_file'] = 'Lanlwytho CSV Ffeil';
