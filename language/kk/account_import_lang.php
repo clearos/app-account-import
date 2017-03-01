3e333209c25d2d09022cdb0028abf014f0390936
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:12:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Шот импорттау Қолданба үтірмен бөлінген мәндер (CSV) пішімінде жүктелген файлын пайдаланып шоттарын сусымалы импортын жүзеге асырады.';
+$lang['account_import_app_name'] = 'шот импорттау';
+$lang['account_import_clear'] = 'айқын';
+$lang['account_import_csv_file'] = 'CSV файлымен';
+$lang['account_import_csv_file_not_found'] = 'CSV файлы табылмады.';
+$lang['account_import_download_csv_template'] = 'CSV Үлгі жүктеп';
+$lang['account_import_import_already_in_progress'] = 'Импорт орындалып жатыр.';
+$lang['account_import_import_complete'] = 'толық импорттаңыз.';
+$lang['account_import_import_log'] = 'Импорт Кіру';
+$lang['account_import_import_users'] = 'Пайдаланушыларды импорттау';
+$lang['account_import_importing_user'] = 'пайдаланушыны импорттау';
+$lang['account_import_no_data_found'] = 'деректер табылған жоқ.';
+$lang['account_import_no_entries'] = 'импорт жазбалар табылған жоқ.';
+$lang['account_import_number_of_records'] = 'Cан көрсеткіштері';
+$lang['account_import_start_import'] = 'Импорттау бастаңыз';
+$lang['account_import_tooltip_csv'] = 'Егер CSV файлы дұрыс пішімделген көз жеткізіңіз - бұл құндылықтар баға белгілеулер Қоса беріліп отырған және үтір ( «1-мән», «2-мән» және т.б.) бөлінген керек еді.';
+$lang['account_import_unable_to_determine_running_state'] = 'импорт жұмыс істеп тұрған болса, анықтау мүмкін емес.';
+$lang['account_import_upload_csv_file'] = 'CSV файлын кері қотару';
