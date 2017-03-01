a12deef46ac634a8c9748f2895d75dd8e96df4ce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:32:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'खाता आयात अनुप्रयोगमा अल्पविराम विभाजित मानहरू (CSV) ढाँचा अपलोड गरिएको फाइल प्रयोग खाता को थोक आयात कार्य गर्दछ।';
+$lang['account_import_app_name'] = 'खाता आयात';
+$lang['account_import_clear'] = 'स्पष्ट';
+$lang['account_import_csv_file'] = 'CSV फाइल';
+$lang['account_import_csv_file_not_found'] = 'CSV फाइल फेला परेन।';
+$lang['account_import_download_csv_template'] = 'CSV डाउनलोड टेम्पलेट';
+$lang['account_import_import_already_in_progress'] = 'आयात प्रगति पहिले देखि नै छ।';
+$lang['account_import_import_complete'] = 'आयात पूरा भयो।';
+$lang['account_import_import_log'] = 'लग आयात';
+$lang['account_import_import_users'] = 'प्रयोगकर्ता आयात';
+$lang['account_import_importing_user'] = 'आयात प्रयोगकर्ता';
+$lang['account_import_no_data_found'] = 'कुनै डाटा भेटिएन।';
+$lang['account_import_no_entries'] = 'कुनै आयात प्रविष्टिहरू फेला परेन।';
+$lang['account_import_number_of_records'] = 'रेकर्ड संख्या';
+$lang['account_import_start_import'] = 'सुरु आयात';
+$lang['account_import_tooltip_csv'] = 'तपाईँको CSV फाइल ठीक ढाँचा छ सुनिश्चित - यो मान कोटेशन मा संलग्न र एक अल्पविराम ( "मान 1", "मान 2" आदि) द्वारा विभाजित हुनुपर्छ।';
+$lang['account_import_unable_to_determine_running_state'] = 'आयात चलिरहेको छ भने निर्धारण गर्न सकिएन।';
+$lang['account_import_upload_csv_file'] = 'CSV फाइल अपलोड गर्नुहोस्';
