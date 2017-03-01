a7699852113224f33c5f9baeafbbaa4eb9f2fccd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:21:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Konta Import app veic lielāko importu kontu, izmantojot augšupielādēto failu komatatdalītu vērtību (CSV) formātā.';
+$lang['account_import_app_name'] = 'konts Import';
+$lang['account_import_clear'] = 'skaidrs';
+$lang['account_import_csv_file'] = 'CSV File';
+$lang['account_import_csv_file_not_found'] = 'CSV fails nav atrasts.';
+$lang['account_import_download_csv_template'] = 'Lejupielādēt CSV veidni';
+$lang['account_import_import_already_in_progress'] = 'Import jau notiek.';
+$lang['account_import_import_complete'] = 'Importēt pabeigta.';
+$lang['account_import_import_log'] = 'Import Log';
+$lang['account_import_import_users'] = 'importa lietotāji';
+$lang['account_import_importing_user'] = 'importēšana lietotāju';
+$lang['account_import_no_data_found'] = 'Nav datu atrasts.';
+$lang['account_import_no_entries'] = 'Nav importa ieraksti netika atrasts.';
+$lang['account_import_number_of_records'] = 'Ierakstu skaits';
+$lang['account_import_start_import'] = 'Start Import';
+$lang['account_import_tooltip_csv'] = 'Pārliecinieties, ka jūsu CSV fails ir formatēts pareizi - tai jābūt vērtībām ieliktas kotāciju un atdalīti ar komatu ( "VALUE1", "VALUE2" uc).';
+$lang['account_import_unable_to_determine_running_state'] = 'Nevar noteikt, vai importa darbojas.';
+$lang['account_import_upload_csv_file'] = 'Augšupielāde CSV failu';
