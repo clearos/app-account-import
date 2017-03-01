8e779b1b68639b2940ba82730a3348f441866cfb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:11:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'ანგარიშის იმპორტი app ასრულებს ნაყარი იმპორტი ანგარიშების გამოყენებით ატვირთული ფაილი მძიმით გამოყოფილი მნიშვნელობები (CSV) ფორმატში.';
+$lang['account_import_app_name'] = 'ანგარიში იმპორტ';
+$lang['account_import_clear'] = 'Clear';
+$lang['account_import_csv_file'] = 'CSV ფაილი';
+$lang['account_import_csv_file_not_found'] = 'CSV ფაილი ვერ მოიძებნა.';
+$lang['account_import_download_csv_template'] = 'ჩამოტვირთვა CSV თარგი';
+$lang['account_import_import_already_in_progress'] = 'იმპორტი უკვე მიმდინარეობს.';
+$lang['account_import_import_complete'] = 'იმპორტი სრული.';
+$lang['account_import_import_log'] = 'იმპორტის ჟურნალი';
+$lang['account_import_import_users'] = 'იმპორტი მომხმარებელი';
+$lang['account_import_importing_user'] = 'იმპორტი შესახებ';
+$lang['account_import_no_data_found'] = 'მონაცემი ვერ მოიძებნა.';
+$lang['account_import_no_entries'] = 'საიმპორტო მასალა იქნა ნაპოვნი.';
+$lang['account_import_number_of_records'] = 'პუნქტების ჩანაწერები';
+$lang['account_import_start_import'] = 'დაწყება იმპორტი';
+$lang['account_import_tooltip_csv'] = 'დარწმუნდით, რომ თქვენი CSV ფაილი ფორმატირებული სწორად - მას უნდა ჰქონდეს ღირებულებების ჩასმული ციტატები და გამოყოფილი მძიმით ( "value1", "value2" და ა.შ.).';
+$lang['account_import_unable_to_determine_running_state'] = 'შეუძლებელია იმის დადგენა, თუ იმპორტი გაშვებული.';
+$lang['account_import_upload_csv_file'] = 'ატვირთვა CSV ფაილი';
