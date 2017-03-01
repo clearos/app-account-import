2134b8c5b7ea914b2d402f39bb1eaf22f4f31c86
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:50:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Tilin Tuo sovellus suorittaa pääosa tuonnista tilejä käyttämällä lähetetyn tiedoston pilkuilla erotetut arvot (CSV) -muodossa.';
+$lang['account_import_app_name'] = 'Tilin Tuo';
+$lang['account_import_clear'] = 'Asia selvä';
+$lang['account_import_csv_file'] = 'CSV File';
+$lang['account_import_csv_file_not_found'] = 'CSV tiedostoa ei löydy.';
+$lang['account_import_download_csv_template'] = 'Lataa CSV malli';
+$lang['account_import_import_already_in_progress'] = 'Tuo on jo käynnissä.';
+$lang['account_import_import_complete'] = 'Tuo valmis.';
+$lang['account_import_import_log'] = 'Tuo Log';
+$lang['account_import_import_users'] = 'Tuo käyttäjiä';
+$lang['account_import_importing_user'] = 'tuominen käyttäjä';
+$lang['account_import_no_data_found'] = 'Dataa ei löytynyt.';
+$lang['account_import_no_entries'] = 'Ei tuonti merkintöjä ei löytynyt.';
+$lang['account_import_number_of_records'] = 'Lukumäärä Records';
+$lang['account_import_start_import'] = 'Aloita tuonti';
+$lang['account_import_tooltip_csv'] = 'Varmista CSV tiedosto on muotoiltu oikein - sen tulisi olla arvot sitaateissa ja erotetaan toisistaan ​​pilkulla ( &#39;arvo1 "," arvo2 "jne.).';
+$lang['account_import_unable_to_determine_running_state'] = 'Pysty määrittämään, jos tuonti on käynnissä.';
+$lang['account_import_upload_csv_file'] = 'Lähetä CSV File';
