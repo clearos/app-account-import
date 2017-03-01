149720b8d648acbad82ead9dc8d54710b8e267e9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:48:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'La aplicación cuenta de importación realiza la importación masiva de cuentas utilizando un archivo cargado en formato de valores separados por comas (CSV).';
+$lang['account_import_app_name'] = 'importación cuenta';
+$lang['account_import_clear'] = 'Claro';
+$lang['account_import_csv_file'] = 'archivo CSV';
+$lang['account_import_csv_file_not_found'] = 'archivo CSV no encontrado.';
+$lang['account_import_download_csv_template'] = 'Descargar la plantilla CSV';
+$lang['account_import_import_already_in_progress'] = 'Importación ya está en progreso.';
+$lang['account_import_import_complete'] = 'Importación completa.';
+$lang['account_import_import_log'] = 'Importar registro';
+$lang['account_import_import_users'] = 'Importar usuarios';
+$lang['account_import_importing_user'] = 'Importación de usuarios';
+$lang['account_import_no_data_found'] = 'Datos no encontrados.';
+$lang['account_import_no_entries'] = 'No se han encontrado entradas de las importaciones.';
+$lang['account_import_number_of_records'] = 'Número de registros';
+$lang['account_import_start_import'] = 'Iniciar importación';
+$lang['account_import_tooltip_csv'] = 'Asegúrese de que su archivo CSV tiene el formato correcto - se debe tener valores encerrada entre comillas y separados por una coma ( "valor1", "valor2", etc.).';
+$lang['account_import_unable_to_determine_running_state'] = 'No se puede determinar si la importación se está ejecutando.';
+$lang['account_import_upload_csv_file'] = 'Sube un archivo CSV';
