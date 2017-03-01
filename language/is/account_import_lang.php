a2646e36254dd179b65b4ab2a028175ad7f1f185
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:07:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'The Account Import app virkar magn innflutning á reikninga með því að nota skrá innsend í gildi aðskilin með kommum (CSV) sniði.';
+$lang['account_import_app_name'] = 'Reikningur Import';
+$lang['account_import_clear'] = 'Hreinsa';
+$lang['account_import_csv_file'] = 'CSV skrá';
+$lang['account_import_csv_file_not_found'] = 'CSV skrá fannst ekki.';
+$lang['account_import_download_csv_template'] = 'Sækja CSV sniðmát';
+$lang['account_import_import_already_in_progress'] = 'Innflutningur er nú þegar í gangi.';
+$lang['account_import_import_complete'] = 'Innflutningi lokið.';
+$lang['account_import_import_log'] = 'innflutningur Log';
+$lang['account_import_import_users'] = 'Flytja notendur';
+$lang['account_import_importing_user'] = 'Flyt notanda';
+$lang['account_import_no_data_found'] = 'Engin gögn fundust.';
+$lang['account_import_no_entries'] = 'Engin innflutningur færslur fundust.';
+$lang['account_import_number_of_records'] = 'Fjöldi Records';
+$lang['account_import_start_import'] = 'Hefja innflutning';
+$lang['account_import_tooltip_csv'] = 'Tryggja CSV skrá er sniðinn almennilega - það ætti að hafa gildi fylgir í tilvitnanir og aðskilin með kommu ( "VALUE1", "VALUE2" o.fl.).';
+$lang['account_import_unable_to_determine_running_state'] = 'Ekki er hægt að ákvarða hvort innflutningur er í gangi.';
+$lang['account_import_upload_csv_file'] = 'Hlaða CSV skrá';
