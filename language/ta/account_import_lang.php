de169fe50d1219db424817321349cc32f504474a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:53:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'கணக்கு இறக்குமதி பயன்பாட்டை கமா பிரிக்கப்பட்ட மதிப்புகள் (CSV) வடிவத்தில் ஒரு பதிவேற்றிய கோப்பை பயன்படுத்தி கணக்குகள் மொத்தமாக இறக்குமதி செய்கிறது.';
+$lang['account_import_app_name'] = 'கணக்கு இறக்குமதி';
+$lang['account_import_clear'] = 'தெளிவு';
+$lang['account_import_csv_file'] = 'CSV கோப்பை';
+$lang['account_import_csv_file_not_found'] = 'CSV கோப்பு காணப்படவில்லை.';
+$lang['account_import_download_csv_template'] = 'கோப்பை டெம்ப்ளேட் பதிவிறக்க';
+$lang['account_import_import_already_in_progress'] = 'இறக்குமதி ஏற்கனவே நடந்து கொண்டிருக்கிறது.';
+$lang['account_import_import_complete'] = 'இறக்குமதி நிறைவுற்றது.';
+$lang['account_import_import_log'] = 'இறக்குமதி பதிகை';
+$lang['account_import_import_users'] = 'இறக்குமதி பயனர்கள்';
+$lang['account_import_importing_user'] = 'இறக்குமதி பயனர்';
+$lang['account_import_no_data_found'] = 'வேறு தகவல்கள் இல்லை.';
+$lang['account_import_no_entries'] = 'இல்லை இறக்குமதி உள்ளீடுகளை காணப்படவில்லை.';
+$lang['account_import_number_of_records'] = 'பதிவுகளை எண்ணிக்கை';
+$lang['account_import_start_import'] = 'தொடக்க இறக்குமதி';
+$lang['account_import_tooltip_csv'] = 'உங்கள் கோப்பில் ஒழுங்காக வடிவமைக்க உறுதி - அது மதிப்புகள் மேற்கோள்கள் இணைக்கப்பட்டு மற்றும் ஒரு கமா ( "VALUE1", "VALUE2" முதலியன) பிரிக்கப்பட்ட வேண்டும்.';
+$lang['account_import_unable_to_determine_running_state'] = 'இறக்குமதி இயங்கும் என்பதைத் தீர்மானிக்க முடியவில்லை.';
+$lang['account_import_upload_csv_file'] = 'CSV கோப்பைப் பதிவேற்று';
