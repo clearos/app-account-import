7ecf0a353a9a2b341b65010fe7fbcab94d92de6e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:42:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Рачун Увоз апликација врши групно увоз налога коришћењем постављена датотека у вредностима одвојеним зарезима (ЦСВ) формату.';
+$lang['account_import_app_name'] = 'рачун Увоз';
+$lang['account_import_clear'] = 'Јасно';
+$lang['account_import_csv_file'] = 'ЦСВ датотека';
+$lang['account_import_csv_file_not_found'] = 'ЦСВ датотека није пронађена.';
+$lang['account_import_download_csv_template'] = 'Довнлоад ЦСВ шаблон';
+$lang['account_import_import_already_in_progress'] = 'Увоз је већ у току.';
+$lang['account_import_import_complete'] = 'Увоз је завршен.';
+$lang['account_import_import_log'] = 'uvoz се';
+$lang['account_import_import_users'] = 'импорт корисника';
+$lang['account_import_importing_user'] = 'Увоз корисника';
+$lang['account_import_no_data_found'] = 'Није пронађен ниједан Подаци.';
+$lang['account_import_no_entries'] = 'Нема увоза уноса је пронађено.';
+$lang['account_import_number_of_records'] = 'Број Рецордс';
+$lang['account_import_start_import'] = 'старт Увоз';
+$lang['account_import_tooltip_csv'] = 'Уверите се ЦСВ датотека правилно форматиран - требало би да имају вредности затворени у цитатима и раздвојене зарезом ( "ВАЛУЕ1", "ВАЛУЕ2" итд).';
+$lang['account_import_unable_to_determine_running_state'] = 'Није могуће да се утврди да ли увоза ради.';
+$lang['account_import_upload_csv_file'] = 'Уплоад ЦСВ датотеку';
