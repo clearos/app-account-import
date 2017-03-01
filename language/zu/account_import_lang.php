df7bab0d71cf7d4fa0bcbb3580833b6840aca406
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:56:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Insiza-Akhawunti Ngenisa oyenza yokungenisa omningi ama-akhawunti usebenzisa i ifayela alayishwe e likhefana ehlukanisiwe amagugu (CSV) yohlelo.';
+$lang['account_import_app_name'] = 'Ngenisa i-Akhawunti';
+$lang['account_import_clear'] = 'Sula';
+$lang['account_import_csv_file'] = 'CSV File';
+$lang['account_import_csv_file_not_found'] = 'Ifayela CSV ayitholakalanga.';
+$lang['account_import_download_csv_template'] = 'Thwebula CSV Isifanekiso';
+$lang['account_import_import_already_in_progress'] = 'Ngenisa isaqhubeka.';
+$lang['account_import_import_complete'] = 'Ukungenisa kuqedile.';
+$lang['account_import_import_log'] = 'Ngenisa Log';
+$lang['account_import_import_users'] = 'Ngenisa Abasebenzisi';
+$lang['account_import_importing_user'] = 'Iyangenisa umsebenzisi';
+$lang['account_import_no_data_found'] = 'Ayikho idatha etholakele.';
+$lang['account_import_no_entries'] = 'Akukho ukungenisa okufakiwe atholakele.';
+$lang['account_import_number_of_records'] = 'Number of Records';
+$lang['account_import_start_import'] = 'Qala Ngenisa';
+$lang['account_import_tooltip_csv'] = '• Ukuqinisekisa Ifayela yakho CSV simiswe kahle - kufanele babe amagugu ebiyelwe izingcaphuno kanye ahlukaniswe a likhefana ( "inani1", "inani2" njll).';
+$lang['account_import_unable_to_determine_running_state'] = 'Ayikwazi ukunquma uma ukungenisa iyasebenza.';
+$lang['account_import_upload_csv_file'] = 'Layisha CSV File';
