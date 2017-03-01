66d23eed67b6a57255e76a0e26063fbfc01d19c1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:29:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Aplikasi Akaun Import melakukan import sebahagian besar daripada akaun menggunakan fail yang dimuat naik dalam format nilai dipisahkan koma (CSV).';
+$lang['account_import_app_name'] = 'Import akaun';
+$lang['account_import_clear'] = 'Clear';
+$lang['account_import_csv_file'] = 'Fail CSV';
+$lang['account_import_csv_file_not_found'] = 'fail CSV tidak dijumpai.';
+$lang['account_import_download_csv_template'] = 'Muat turun CSV Template';
+$lang['account_import_import_already_in_progress'] = 'Import sedang dijalankan.';
+$lang['account_import_import_complete'] = 'Import selesai.';
+$lang['account_import_import_log'] = 'import Log';
+$lang['account_import_import_users'] = 'Pengguna import';
+$lang['account_import_importing_user'] = 'mengimport pengguna';
+$lang['account_import_no_data_found'] = 'Tiada data.';
+$lang['account_import_no_entries'] = 'Tiada catatan import ditemui.';
+$lang['account_import_number_of_records'] = 'Bilangan Records';
+$lang['account_import_start_import'] = 'Mula Import';
+$lang['account_import_tooltip_csv'] = 'Pastikan fail CSV anda diformat dengan betul - ia harus mempunyai nilai-nilai yang terdapat di dalam petikan dan dipisahkan oleh koma ( "nilai1", "nilai2" dan lain-lain).';
+$lang['account_import_unable_to_determine_running_state'] = 'Tidak dapat menentukan sama ada import sedang berjalan.';
+$lang['account_import_upload_csv_file'] = 'Memuat CSV Fail';
