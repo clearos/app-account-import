9662204f0c690d114b7d8513cfd049477694af0d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:46:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Account Import app udfører hovedparten import af konti ved hjælp en uploadet fil i kommaseparerede værdier (CSV).';
+$lang['account_import_app_name'] = 'konto Import';
+$lang['account_import_clear'] = 'Klar';
+$lang['account_import_csv_file'] = 'CSV-fil';
+$lang['account_import_csv_file_not_found'] = 'CSV-fil ikke fundet.';
+$lang['account_import_download_csv_template'] = 'Hent CSV skabelon';
+$lang['account_import_import_already_in_progress'] = 'Import er allerede i gang.';
+$lang['account_import_import_complete'] = 'Import fuldført.';
+$lang['account_import_import_log'] = 'Import Log';
+$lang['account_import_import_users'] = 'Importer brugere';
+$lang['account_import_importing_user'] = 'Import af brugeren';
+$lang['account_import_no_data_found'] = 'Ingen data fundet.';
+$lang['account_import_no_entries'] = 'Ingen oplysningerne import blev fundet.';
+$lang['account_import_number_of_records'] = 'Antal Records';
+$lang['account_import_start_import'] = 'Start Import';
+$lang['account_import_tooltip_csv'] = 'Sørg din CSV-fil er formateret korrekt - det bør have værdier omsluttet af citater og adskilt af et komma ( "værdi1", "værdi2" osv).';
+$lang['account_import_unable_to_determine_running_state'] = 'Kan ikke afgøre, om import kører.';
+$lang['account_import_upload_csv_file'] = 'Upload CSV-fil';
