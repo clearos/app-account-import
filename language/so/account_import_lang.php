8dbc1627f4aacb432f9da12b7e0d41590a858d73
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:46:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'The app Account Import qabata dejinta bulk of xisaab isticmaalaya file ah uploaded ee comma kala maqan qiimaha format (CSV).';
+$lang['account_import_app_name'] = 'Import Account';
+$lang['account_import_clear'] = 'Clear';
+$lang['account_import_csv_file'] = 'CSV File';
+$lang['account_import_csv_file_not_found'] = 'file CSV ma ay helin.';
+$lang['account_import_download_csv_template'] = 'Download CSV Template';
+$lang['account_import_import_already_in_progress'] = 'Import horeba waa in horumarka.';
+$lang['account_import_import_complete'] = 'Import complete.';
+$lang['account_import_import_log'] = 'Import Log';
+$lang['account_import_import_users'] = 'Users Import';
+$lang['account_import_importing_user'] = 'user Keenista';
+$lang['account_import_no_data_found'] = 'data No helay.';
+$lang['account_import_no_entries'] = 'No entries dejinta la helay.';
+$lang['account_import_number_of_records'] = 'Number of Records';
+$lang['account_import_start_import'] = 'Start Import';
+$lang['account_import_tooltip_csv'] = 'Hubi in aad file CSV si fiican loo formatted - waa in ay leeyihiin qiimaha ku lifaaqan ee sheegyada iyo kala comma ( "value1", "value2" iwm).';
+$lang['account_import_unable_to_determine_running_state'] = 'Lama si loo ogaado haddii dejinta waxaa socda.';
+$lang['account_import_upload_csv_file'] = 'Kasoorar File CSV';
