9ac2acc279f57a8f9fcd6d94b5af5b359b414227
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:50:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Nu Akun Impor app ngalakukeun impor bulk akun maké gambar nu dimuat dina koma dipisahkeun nilai (CSV) format.';
+$lang['account_import_app_name'] = 'akun Impor';
+$lang['account_import_clear'] = 'atra';
+$lang['account_import_csv_file'] = 'CSV File';
+$lang['account_import_csv_file_not_found'] = 'file CSV teu kapanggih.';
+$lang['account_import_download_csv_template'] = 'Ngundeur CSV Citakan';
+$lang['account_import_import_already_in_progress'] = 'Impor geus di kamajuan.';
+$lang['account_import_import_complete'] = 'Impor lengkep.';
+$lang['account_import_import_log'] = 'impor Log';
+$lang['account_import_import_users'] = 'Pamaké impor';
+$lang['account_import_importing_user'] = 'Importing pamaké';
+$lang['account_import_no_data_found'] = 'No data kapanggih.';
+$lang['account_import_no_entries'] = 'No éntri impor anu kapanggih.';
+$lang['account_import_number_of_records'] = 'Jumlah Records';
+$lang['account_import_start_import'] = 'mimitian Impor';
+$lang['account_import_tooltip_csv'] = 'Mastikeun file CSV Anjeun formatna bener - eta kudu boga nilai enclosed dina cutatan jeung dipisahkeun ku koma ( "value1", "value2" jsb).';
+$lang['account_import_unable_to_determine_running_state'] = 'Teu bisa nangtukeun lamun impor anu ngajalankeun.';
+$lang['account_import_upload_csv_file'] = 'Munggah CSV File';
