e3d2f92ad57644f9affd7beb40fe640dce4e7c16
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:40:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'The app Account Tengani amachita zambiri chiyani pa nkhani ntchito file ndi ndakweza mu mtundu koma wosiyana mfundo (CSV).';
+$lang['account_import_app_name'] = 'Tengani nkhani';
+$lang['account_import_clear'] = 'Chotsani';
+$lang['account_import_csv_file'] = 'CSV failo';
+$lang['account_import_csv_file_not_found'] = 'CSV wapamwamba sanapezeke.';
+$lang['account_import_download_csv_template'] = 'Koperani CSV Chinsinsi';
+$lang['account_import_import_already_in_progress'] = 'Tengani kale mu patsogolo.';
+$lang['account_import_import_complete'] = 'Tengani wathunthu.';
+$lang['account_import_import_log'] = 'Tengani Log';
+$lang['account_import_import_users'] = 'Tengani Ogwiritsa';
+$lang['account_import_importing_user'] = 'yoti wosuta';
+$lang['account_import_no_data_found'] = 'No deta anapeza.';
+$lang['account_import_no_entries'] = 'No zolemba nji anapezeka.';
+$lang['account_import_number_of_records'] = 'Number of Records';
+$lang['account_import_start_import'] = 'Start Tengani';
+$lang['account_import_tooltip_csv'] = 'Kuonetsetsa file wanu CSV ali Onani m&#39;magazini bwino - tiyenera kukhala ndi makhalidwe akachita m&#39;mabuku ndi wosiyana ndi koma ( "value1", "value2" etc.).';
+$lang['account_import_unable_to_determine_running_state'] = 'Akulephera kudziwa ngati nji ikutha.';
+$lang['account_import_upload_csv_file'] = 'Kukweza CSV failo';
