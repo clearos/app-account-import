368208b2269ebd976d8f1e79cad05d92f1f56619
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:54:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'ఖాతా దిగుమతి అనువర్తనం కామాతో వేరుచేయబడిన వేల్యూస్ (CSV) ఫార్మాట్ లో ఒక అప్లోడ్ ఫైలు ఉపయోగించి ఖాతాల సమూహ దిగుమతి అమలు చేస్తుంది.';
+$lang['account_import_app_name'] = 'ఖాతా దిగుమతి';
+$lang['account_import_clear'] = 'ప్రశాంతంగా';
+$lang['account_import_csv_file'] = 'CSV ఫైల్';
+$lang['account_import_csv_file_not_found'] = 'CSV ఫైల్ కనుగొనబడలేదు.';
+$lang['account_import_download_csv_template'] = 'CSV మూస';
+$lang['account_import_import_already_in_progress'] = 'దిగుమతి ఇప్పటికే ప్రోగ్రెస్లో ఉంది.';
+$lang['account_import_import_complete'] = 'దిగుమతి పూర్తయింది.';
+$lang['account_import_import_log'] = 'దిగుమతి లోనికి ప్రవేశించండి';
+$lang['account_import_import_users'] = 'దిగుమతి వినియోగదారులు';
+$lang['account_import_importing_user'] = 'దిగుమతి యూజర్';
+$lang['account_import_no_data_found'] = 'డేటా కనుగొనబడలేదు.';
+$lang['account_import_no_entries'] = 'ఎటువంటి దిగుమతి ఎంట్రీలు దొరకలేదు.';
+$lang['account_import_number_of_records'] = 'రికార్డ్స్ సంఖ్య';
+$lang['account_import_start_import'] = 'ప్రారంభం దిగుమతి';
+$lang['account_import_tooltip_csv'] = 'మీ CSV ఫైల్ సక్రమంగా ఫార్మాట్ చూచుకొనుము - ఇది విలువలకు ఉల్లేఖనాలు చుట్టూ మరియు ఒక కామా ( "విలువ 1", "విలువ 2" మొదలైనవి) వేరు ఉండాలి.';
+$lang['account_import_unable_to_determine_running_state'] = 'దిగుమతి నడుస్తుంటే గుర్తించలేకపోయింది.';
+$lang['account_import_upload_csv_file'] = 'CSV ఫైల్ అప్లోడ్';
