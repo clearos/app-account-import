59bdb7bd3ddce508de2dffcee681c1fd77bbb6dc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:47:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'The app e Account Import etsa bongata bohlokoa ba litlaleho sebelisa okotisi uploaded le popego phelelwana arohana makgabane (CSV).';
+$lang['account_import_app_name'] = 'Import Account';
+$lang['account_import_clear'] = 'e hlakileng';
+$lang['account_import_csv_file'] = 'CSV File';
+$lang['account_import_csv_file_not_found'] = 'CSV faele ha le a fumanwa.';
+$lang['account_import_download_csv_template'] = 'Download CSV Template';
+$lang['account_import_import_already_in_progress'] = 'Import ke se a ntse a tsoela pele.';
+$lang['account_import_import_complete'] = 'Import ho feletseng.';
+$lang['account_import_import_log'] = 'Import Log';
+$lang['account_import_import_users'] = 'Import Users';
+$lang['account_import_importing_user'] = 'kenya user want-meet.ru';
+$lang['account_import_no_data_found'] = 'No ya data fumanoa.';
+$lang['account_import_no_entries'] = 'No nyako bohlokoa ile a fumanoa.';
+$lang['account_import_number_of_records'] = 'Palo ea Records';
+$lang['account_import_start_import'] = 'Start Import';
+$lang['account_import_tooltip_csv'] = 'Etsa bonnete ba faele ya hao CSV e boitsebiso bo hlophisitsoeng ka tsela e loketseng - e lokela ho ba le makgabane kenyeleditsweng a qotsitsoe le arohantsoe ke e phelelwana ( "value1", "value2" jj.)';
+$lang['account_import_unable_to_determine_running_state'] = 'Ha ho kgonehe ho fumana hore na bohlokoa e matha.';
+$lang['account_import_upload_csv_file'] = 'Kenya CSV File';
