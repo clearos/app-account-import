bca1c28402e1de411bee770a99e021e3fe688896
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:04:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Comhlíonann an app Iompórtáil Cuntas allmhairiú mórchóir cuntas ag baint úsáide as comhad uaslódáilte in camóga eatarthu luachanna (CSV) formáid.';
+$lang['account_import_app_name'] = 'Iompórtáil Cuntas';
+$lang['account_import_clear'] = 'Clear';
+$lang['account_import_csv_file'] = 'CSV Comhad';
+$lang['account_import_csv_file_not_found'] = 'comhad CSV gan aimsiú.';
+$lang['account_import_download_csv_template'] = 'Íosluchtaigh CSV Teimpléad';
+$lang['account_import_import_already_in_progress'] = 'Iompórtáil ar siúl cheana féin.';
+$lang['account_import_import_complete'] = 'Iompórtáil iomlán.';
+$lang['account_import_import_log'] = 'Iompórtáil Logáil';
+$lang['account_import_import_users'] = 'Úsáideoirí Iompórtáil';
+$lang['account_import_importing_user'] = 'úsáideoir Iompórtáil';
+$lang['account_import_no_data_found'] = 'Aon sonraí aimsíodh aon rud.';
+$lang['account_import_no_entries'] = 'Níor aimsíodh aon iontrálacha allmhairiú.';
+$lang['account_import_number_of_records'] = 'Líon na dTaifead';
+$lang['account_import_start_import'] = 'Iompórtáil Start';
+$lang['account_import_tooltip_csv'] = 'Cinntigh go bhfuil do chomhad CSV formáidithe i gceart - ba cheart go mbeadh na luachanna iamh in luachana agus scartha le camóga ( "value1", "value2" srl).';
+$lang['account_import_unable_to_determine_running_state'] = 'Níféidir amach an bhfuil allmhairí ag rith.';
+$lang['account_import_upload_csv_file'] = 'Uaslódáil CSV Comhad';
