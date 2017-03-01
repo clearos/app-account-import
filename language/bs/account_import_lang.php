1e37270b8ad0b8209e428604ea4d0d263a0f1380
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:35:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Računa Import aplikacija obavlja najveći uvoz računa pomoću dodane datoteke u zarezom odvojene vrijednosti (CSV) formatu.';
+$lang['account_import_app_name'] = 'račun Import';
+$lang['account_import_clear'] = 'jasno';
+$lang['account_import_csv_file'] = 'CSV datoteke';
+$lang['account_import_csv_file_not_found'] = 'CSV datoteka nije pronađena.';
+$lang['account_import_download_csv_template'] = 'Preuzimanje CSV Template';
+$lang['account_import_import_already_in_progress'] = 'Uvoz je već u tijeku.';
+$lang['account_import_import_complete'] = 'Uvoz potpuna.';
+$lang['account_import_import_log'] = 'uvoz Prijavite';
+$lang['account_import_import_users'] = 'uvoz Korisnici';
+$lang['account_import_importing_user'] = 'Uvoz korisnik';
+$lang['account_import_no_data_found'] = 'Nema podataka.';
+$lang['account_import_no_entries'] = 'Nije uvoz nije pronađeno.';
+$lang['account_import_number_of_records'] = 'Broj Records';
+$lang['account_import_start_import'] = 'Start Import';
+$lang['account_import_tooltip_csv'] = 'Osigurajte CSV datoteka pravilno formatiran - treba imati vrijednosti zatvoren u citatima i odvojeni zarezom ( "vrednost1", "vrednost2", itd).';
+$lang['account_import_unable_to_determine_running_state'] = 'U nemogućnosti da se utvrdi da li je uvoz radi.';
+$lang['account_import_upload_csv_file'] = 'Upload CSV datoteke';
