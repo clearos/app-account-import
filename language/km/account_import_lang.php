0ac17478db656f6d3dcafc74e76392f002413130
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:14:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'គណនីកម្មវិធីដំណើរការនាំចូលនាំចូលភាគច្រើននៃគណនីដោយប្រើឯកសារដែលបានផ្ទុកឡើងក្នុងទ្រង់ទ្រាយតម្លៃបំបែកដោយសញ្ញាក្បៀស (CSV) ។';
+$lang['account_import_app_name'] = 'នាំចូលគណនី';
+$lang['account_import_clear'] = 'ការច្បាស់លាស់';
+$lang['account_import_csv_file'] = 'ឯកសារ CSV';
+$lang['account_import_csv_file_not_found'] = 'រកមិនឃើញឯកសារ CSV ។';
+$lang['account_import_download_csv_template'] = 'ទាញយកឯកសារ CSV ទំព័រគំរូ';
+$lang['account_import_import_already_in_progress'] = 'សំខាន់គឺរួចហើយនៅក្នុងការរីកចំរើន។';
+$lang['account_import_import_complete'] = 'នាំចូលពេញលេញ។';
+$lang['account_import_import_log'] = 'នាំចូលចូល';
+$lang['account_import_import_users'] = 'ការនាំចូលអ្នកប្រើ';
+$lang['account_import_importing_user'] = 'ការនាំចូលរបស់អ្នកប្រើ';
+$lang['account_import_no_data_found'] = 'រកមិនឃើញទិន្នន័យ។';
+$lang['account_import_no_entries'] = 'គ្មានធាតុដែលបាននាំចូលត្រូវបានរកឃើញ។';
+$lang['account_import_number_of_records'] = 'ចំនួននៃកំណត់ត្រា';
+$lang['account_import_start_import'] = 'ចាប់ផ្តើមនាំចូល';
+$lang['account_import_tooltip_csv'] = 'ធានាឱ្យបាននូវឯកសារ CSV របស់អ្នកត្រូវបានធ្វើទ្រង់ទ្រាយត្រឹមត្រូវ - វាគួរតែមានតម្លៃរុំព័ទ្ធក្នុងតារាងតម្លៃនិងការបំបែកដោយសញ្ញាក្បៀស ( "តម្លៃ 1", "2" ជាដើម) ។';
+$lang['account_import_unable_to_determine_running_state'] = 'មិនអាចកំណត់បានប្រសិនបើការនាំចូលកំពុងរត់។';
+$lang['account_import_upload_csv_file'] = 'ផ្ទុកឡើងឯកសារ CSV';
