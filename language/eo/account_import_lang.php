3329cc297300aa350d2cb68a35fb36cf6addb22c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:47:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'La Konto Importi app elfaras dika importado de rakontas uzante alŝutita dosiero en komo apartigis valorojn (CSV) formato.';
+$lang['account_import_app_name'] = 'konto Importado';
+$lang['account_import_clear'] = 'klara';
+$lang['account_import_csv_file'] = 'CSV Dosiero';
+$lang['account_import_csv_file_not_found'] = 'CSV-dosiero ne troviĝis.';
+$lang['account_import_download_csv_template'] = 'Elŝutu CSV Ŝablono';
+$lang['account_import_import_already_in_progress'] = 'Importado estas jam en progreso.';
+$lang['account_import_import_complete'] = 'Import kompleta.';
+$lang['account_import_import_log'] = 'import Ensalutu';
+$lang['account_import_import_users'] = 'import Uzantoj';
+$lang['account_import_importing_user'] = 'Importante uzanto';
+$lang['account_import_no_data_found'] = 'Neniu datumo trovita.';
+$lang['account_import_no_entries'] = 'Neniu import kontribuoj estis trovitaj.';
+$lang['account_import_number_of_records'] = 'Numeron de Rekordoj';
+$lang['account_import_start_import'] = 'Komenci importon';
+$lang['account_import_tooltip_csv'] = 'Certigi vian CSV dosiero estas formatita konvene - ĝi devus havi valorojn enfermita en citaĵoj kaj apartigita per komo ( "value1", "value2" ktp).';
+$lang['account_import_unable_to_determine_running_state'] = 'Nekapabla determini se importado kuras.';
+$lang['account_import_upload_csv_file'] = 'Alŝuti CSV Dosiero';
