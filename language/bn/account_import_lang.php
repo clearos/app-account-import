289cbe496fbab7a9e8cb29559011d4bc4ab71c71
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:33:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'অ্যাকাউন্ট আমদানি অ্যাপ্লিকেশন কমা দ্বারা পৃথক মান (CSV-) ফরম্যাটে আপলোড ফাইল ব্যবহার করে অ্যাকাউন্টের বাল্ক আমদানি সঞ্চালিত হবে.';
+$lang['account_import_app_name'] = 'অ্যাকাউন্ট আমদানি';
+$lang['account_import_clear'] = 'পরিষ্কার';
+$lang['account_import_csv_file'] = 'CSV ফাইল';
+$lang['account_import_csv_file_not_found'] = 'CSV ফাইল পাওয়া যায়নি.';
+$lang['account_import_download_csv_template'] = 'CSV ডাউনলোড করুন টেমপ্লেট';
+$lang['account_import_import_already_in_progress'] = 'আমদানি ইতিমধ্যে অগ্রগতি হয়.';
+$lang['account_import_import_complete'] = 'আমদানি সম্পূর্ণ.';
+$lang['account_import_import_log'] = 'আমদানি লগ';
+$lang['account_import_import_users'] = 'আমদানি ব্যবহারকারীরা';
+$lang['account_import_importing_user'] = 'আমদানি হচ্ছে ব্যবহারকারী';
+$lang['account_import_no_data_found'] = 'কোন তথ্য পাওয়া যায়.';
+$lang['account_import_no_entries'] = 'কোন আমদানি এন্ট্রি খুঁজে পাওয়া যায়নি.';
+$lang['account_import_number_of_records'] = 'রেকর্ডস সংখ্যা';
+$lang['account_import_start_import'] = 'আমদানি শুরু';
+$lang['account_import_tooltip_csv'] = 'নিশ্চিত হোন যে আপনার CSV ফাইল সঠিকভাবে ফরম্যাট করা হয় - এটা মান উদ্ধৃতি ঘিরা এবং একটি কমা ( "মান 1", "মান 2" ইত্যাদি) দ্বারা বিভক্ত থাকা উচিত.';
+$lang['account_import_unable_to_determine_running_state'] = 'আমদানি চলমান নির্ধারণ করতে অক্ষম.';
+$lang['account_import_upload_csv_file'] = 'CSV ফাইল আপলোড করুন';
