a94046d014a98ccb37aaa865ca53975c32253116
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:55:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Các ứng dụng nhập tài khoản thực hiện nhập khẩu số lượng lớn các tài khoản sử dụng một tập tin tải lên trong dấu phẩy tách giá trị định dạng (CSV).';
+$lang['account_import_app_name'] = 'nhập tài khoản';
+$lang['account_import_clear'] = 'Trong sáng';
+$lang['account_import_csv_file'] = 'tệp CSV';
+$lang['account_import_csv_file_not_found'] = 'không tìm thấy tập tin CSV.';
+$lang['account_import_download_csv_template'] = 'Tải xuống mẫu CSV';
+$lang['account_import_import_already_in_progress'] = 'Nhập khẩu đã được tiến hành.';
+$lang['account_import_import_complete'] = 'Nhập đầy đủ.';
+$lang['account_import_import_log'] = 'nhập Đăng nhập';
+$lang['account_import_import_users'] = 'người dùng nhập khẩu';
+$lang['account_import_importing_user'] = 'nhập khẩu người dùng';
+$lang['account_import_no_data_found'] = 'Không tìm thấy dữ liệu nào.';
+$lang['account_import_no_entries'] = 'Không có mục nhập khẩu đã được tìm thấy.';
+$lang['account_import_number_of_records'] = 'Số hồ sơ';
+$lang['account_import_start_import'] = 'Bắt đầu nhập';
+$lang['account_import_tooltip_csv'] = 'Đảm bảo tập tin CSV của bạn được định dạng đúng - nó phải có giá trị kèm theo trong danh ngôn và cách nhau bằng dấu phẩy ( "value1", "value2" vv).';
+$lang['account_import_unable_to_determine_running_state'] = 'Không thể xác định nếu nhập khẩu đang chạy.';
+$lang['account_import_upload_csv_file'] = 'Tải lên tập tin CSV';
