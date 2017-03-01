e92e402d95c395ba3e7c8a086bb07368fdac99fb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:55:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'די אַקאַונט ימפּאָרט אַפּ פּערפאָרמס פאַרנעם אַרייַנפיר פון אַקאַונץ ניצן אַ ופּלאָאַדעד טעקע אין קאָמע צעשיידט וואַלועס (קסוו) פֿאָרמאַט.';
+$lang['account_import_app_name'] = 'אַקאַונט ימפּאָרט';
+$lang['account_import_clear'] = 'קלאָר';
+$lang['account_import_csv_file'] = 'קסוו טעקע';
+$lang['account_import_csv_file_not_found'] = 'קסוו טעקע ניט געפֿונען.';
+$lang['account_import_download_csv_template'] = 'דאַונלאָוד קסוו טעמפּלאַטע';
+$lang['account_import_import_already_in_progress'] = 'ימפּאָרט איז שוין אין פּראָגרעס.';
+$lang['account_import_import_complete'] = 'ימפּאָרט גאַנץ.';
+$lang['account_import_import_log'] = 'ימפּאָרט קלאָץ';
+$lang['account_import_import_users'] = 'אַרייַנפיר ניצערס';
+$lang['account_import_importing_user'] = 'ימפּאָרטינג וסערס';
+$lang['account_import_no_data_found'] = 'קיין דאַטן געפֿונען.';
+$lang['account_import_no_entries'] = 'ניט קיין אַרייַנפיר איינסן זענען געפֿונען.';
+$lang['account_import_number_of_records'] = 'נומער פון רעקאָרדס';
+$lang['account_import_start_import'] = 'אָנהייב ימפּאָרט';
+$lang['account_import_tooltip_csv'] = 'ינשור דיין קסוו טעקע איז פֿאָרמאַטירט רעכט - עס זאָל האָבן וואַלועס ענקלאָוזד אין ציטאטן און צעשיידט דורך אַ קאָמע ( "וואַלוע1", "וואַלוע2" אאז"וו).';
+$lang['account_import_unable_to_determine_running_state'] = 'ניט געקענט צו באַשליסן אויב אַרייַנפיר איז פליסנדיק.';
+$lang['account_import_upload_csv_file'] = 'צופֿעליקער קסוו טעקע';
