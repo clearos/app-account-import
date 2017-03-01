d407733c9062bfc424336baef89ff88c558acdaf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:54:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'แอปพลิเคบัญชีนำเข้าดำเนินการนำเข้าเป็นกลุ่มของบัญชีที่ใช้ไฟล์ที่อัปโหลดในคั่นด้วยเครื่องหมายจุลภาค (CSV)';
+$lang['account_import_app_name'] = 'นำเข้าบัญชี';
+$lang['account_import_clear'] = 'ชัดเจน';
+$lang['account_import_csv_file'] = 'ไฟล์ CSV';
+$lang['account_import_csv_file_not_found'] = 'ไม่พบไฟล์ CSV';
+$lang['account_import_download_csv_template'] = 'ดาวน์โหลดเทมเพลต CSV';
+$lang['account_import_import_already_in_progress'] = 'นำเข้าที่มีอยู่แล้วในความคืบหน้า';
+$lang['account_import_import_complete'] = 'นำเข้าที่สมบูรณ์';
+$lang['account_import_import_log'] = 'นำเข้าสู่ระบบ';
+$lang['account_import_import_users'] = 'ผู้ใช้นำเข้า';
+$lang['account_import_importing_user'] = 'ผู้ใช้นำเข้า';
+$lang['account_import_no_data_found'] = 'ไม่พบข้อมูล.';
+$lang['account_import_no_entries'] = 'ไม่มีรายการที่นำเข้าถูกพบ';
+$lang['account_import_number_of_records'] = 'จำนวนของระเบียน';
+$lang['account_import_start_import'] = 'เริ่มต้นนำเข้า';
+$lang['account_import_tooltip_csv'] = 'ตรวจสอบให้แน่ใจไฟล์ CSV ของคุณมีรูปแบบที่ถูกต้อง - มันควรจะมีค่าอยู่ในใบเสนอราคาและคั่นด้วยเครื่องหมายจุลภาค ( "VALUE1", "VALUE2" ฯลฯ )';
+$lang['account_import_unable_to_determine_running_state'] = 'ไม่สามารถที่จะตรวจสอบว่าการนำเข้าทำงาน';
+$lang['account_import_upload_csv_file'] = 'อัปโหลดไฟล์ CSV';
