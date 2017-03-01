07be88c41b713780d80267006ecea3b1fc12b31a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:55:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Hesap İthalat uygulaması virgülle ayrılmış değerler (CSV) biçiminde yüklenen bir dosyayı kullanarak hesaplar Toplu alma gerçekleştirir.';
+$lang['account_import_app_name'] = 'hesap Alma';
+$lang['account_import_clear'] = 'Açık';
+$lang['account_import_csv_file'] = 'CSV Dosya';
+$lang['account_import_csv_file_not_found'] = 'CSV dosyası bulunamadı.';
+$lang['account_import_download_csv_template'] = 'CSV şablonu indirin';
+$lang['account_import_import_already_in_progress'] = 'İthalat zaten devam ediyor.';
+$lang['account_import_import_complete'] = 'İthalat tamamlandı.';
+$lang['account_import_import_log'] = 'İthalat Günlüğü';
+$lang['account_import_import_users'] = 'İthalat Kullanıcılar';
+$lang['account_import_importing_user'] = 'İthalat kullanıcı';
+$lang['account_import_no_data_found'] = 'Veri bulunamadı.';
+$lang['account_import_no_entries'] = 'Hiçbir ithalat girdileri bulunamadı.';
+$lang['account_import_number_of_records'] = 'Kayıt Sayısı';
+$lang['account_import_start_import'] = 'Başlangıç ​​İthalat';
+$lang['account_import_tooltip_csv'] = 'CSV dosyasının düzgün formatlı olduğundan emin olun - bu değerleri tırnak içine ve virgül ( "değer1&#39;e", "değer2" vs.) ile ayrılmış olmalıdır.';
+$lang['account_import_unable_to_determine_running_state'] = 'ithalat çalışıp çalışmadığını belirlemek için açılamıyor.';
+$lang['account_import_upload_csv_file'] = 'CSV Dosya Yükle';
