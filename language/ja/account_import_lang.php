245fffcba479d11dbd4f238b47dcb35c416483c1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:10:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'アカウントのインポートアプリは、カンマ区切り（CSV）形式でアップロードされたファイルを使用してアカウントの一括インポートを実行します。';
+$lang['account_import_app_name'] = 'アカウントのインポート';
+$lang['account_import_clear'] = 'クリア';
+$lang['account_import_csv_file'] = 'CSVファイル';
+$lang['account_import_csv_file_not_found'] = 'CSVファイルが見つかりません。';
+$lang['account_import_download_csv_template'] = 'CSVテンプレートをダウンロード';
+$lang['account_import_import_already_in_progress'] = 'インポートは、すでに進行中です。';
+$lang['account_import_import_complete'] = 'インポートが完了しました。';
+$lang['account_import_import_log'] = 'インポートログ';
+$lang['account_import_import_users'] = 'ユーザーのインポート';
+$lang['account_import_importing_user'] = 'ユーザーのインポート';
+$lang['account_import_no_data_found'] = '何もデータが見つかりませんでした。';
+$lang['account_import_no_entries'] = 'インポートエントリが見つかりませんでした。';
+$lang['account_import_number_of_records'] = 'レコード数';
+$lang['account_import_start_import'] = 'インポート開始';
+$lang['account_import_tooltip_csv'] = 'CSVファイルが正しくフォーマットされているか確認します - それは値が引用符で囲まれ、カンマ（ "値1"、 "値2"など）で区切られている必要があります。';
+$lang['account_import_unable_to_determine_running_state'] = 'インポートが実行されているかどうかを決定することができません。';
+$lang['account_import_upload_csv_file'] = 'CSVファイルをアップロード';
