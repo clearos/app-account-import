05d0022bc5bc6da7a9cedff0132730f943b189cf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:03:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Rekening Impor aplikasi melakukan impor massal dari akun menggunakan file upload di nilai terpisah koma (CSV).';
+$lang['account_import_app_name'] = 'akun Impor';
+$lang['account_import_clear'] = 'Bersih';
+$lang['account_import_csv_file'] = 'file CSV';
+$lang['account_import_csv_file_not_found'] = 'file CSV tidak ditemukan.';
+$lang['account_import_download_csv_template'] = 'Unduh CSV Template';
+$lang['account_import_import_already_in_progress'] = 'Impor sudah berlangsung.';
+$lang['account_import_import_complete'] = 'Impor lengkap.';
+$lang['account_import_import_log'] = 'impor Masuk';
+$lang['account_import_import_users'] = 'Pengguna impor';
+$lang['account_import_importing_user'] = 'mengimpor pengguna';
+$lang['account_import_no_data_found'] = 'Tidak ada data.';
+$lang['account_import_no_entries'] = 'Tidak ada entri impor yang ditemukan.';
+$lang['account_import_number_of_records'] = 'Jumlah Rekaman';
+$lang['account_import_start_import'] = 'Mulai Impor';
+$lang['account_import_tooltip_csv'] = 'Pastikan file CSV Anda diformat dengan benar - itu harus memiliki nilai-nilai tertutup dalam kutipan dan dipisahkan oleh koma ( "value1", "value2" dll).';
+$lang['account_import_unable_to_determine_running_state'] = 'Tidak dapat menentukan apakah impor berjalan.';
+$lang['account_import_upload_csv_file'] = 'Upload File CSV';
