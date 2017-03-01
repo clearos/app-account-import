b2aa96f44830ff96f27126405dbbbb807930627d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:25:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'App Llogaria Import kryen importin pjesa më e madhe e llogarive duke përdorur një file uploaded në të ndara me presje vlerave format (CSV).';
+$lang['account_import_app_name'] = 'Import llogari';
+$lang['account_import_clear'] = 'Qartë';
+$lang['account_import_csv_file'] = 'CSV dokumentit';
+$lang['account_import_csv_file_not_found'] = 'CSV file nuk gjendet.';
+$lang['account_import_download_csv_template'] = 'Shkarko CSV Template';
+$lang['account_import_import_already_in_progress'] = 'Import është tashmë në zhvillim e sipër.';
+$lang['account_import_import_complete'] = 'Import plotë.';
+$lang['account_import_import_log'] = 'Import Identifikohu';
+$lang['account_import_import_users'] = 'Përdoruesit e importit';
+$lang['account_import_importing_user'] = 'Importimi i përdoruesit';
+$lang['account_import_no_data_found'] = 'Nuk ka të dhëna gjetur.';
+$lang['account_import_no_entries'] = 'Asnjë entries importit janë gjetur.';
+$lang['account_import_number_of_records'] = 'Numri i rekordeve';
+$lang['account_import_start_import'] = 'fillimi Import';
+$lang['account_import_tooltip_csv'] = 'Siguruar dosja juaj CSV është formatuar si duhet - duhet të ketë vlera të mbyllur në kuotimin dhe të ndara me presje ( "value1", "value2" etj).';
+$lang['account_import_unable_to_determine_running_state'] = 'Në pamundësi për të përcaktuar nëse importi është i rrjedhshëm.';
+$lang['account_import_upload_csv_file'] = 'Ngarko CSV file';
