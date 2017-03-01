6ab34e9197a499830b60244d5473adb9bbeb1537
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:32:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Дадатак Account Імпарт ажыццяўляе масавы імпарт рахункаў з выкарыстаннем загружанага файла ў фармаце значэння, падзеленыя коскамі (CSV).';
+$lang['account_import_app_name'] = 'Імпарт ўліковага запісу';
+$lang['account_import_clear'] = 'ясна';
+$lang['account_import_csv_file'] = 'CSV-файл';
+$lang['account_import_csv_file_not_found'] = 'CSV файл не знойдзены.';
+$lang['account_import_download_csv_template'] = 'Загрузіць CSV-шаблон';
+$lang['account_import_import_already_in_progress'] = 'Імпарт ужо ідзе.';
+$lang['account_import_import_complete'] = 'Імпарт завершаны.';
+$lang['account_import_import_log'] = 'імпарт Уваход';
+$lang['account_import_import_users'] = 'імпарт карыстальнікаў';
+$lang['account_import_importing_user'] = 'імпарт карыстальнікаў';
+$lang['account_import_no_data_found'] = 'Дадзеных няма.';
+$lang['account_import_no_entries'] = 'Не знойдзена ніводнага імпарту запісаў.';
+$lang['account_import_number_of_records'] = 'колькасць запісаў';
+$lang['account_import_start_import'] = 'Start Import';
+$lang['account_import_tooltip_csv'] = 'Пераканайцеся, што ваш файл CSV адфарматаваны належным чынам - яно павінна мець значэння, зняволеныя ў двукоссі і падзеленых коскай ( "value1", "value2" і г.д.).';
+$lang['account_import_unable_to_determine_running_state'] = 'Немагчыма вызначыць, калі імпарт працуе.';
+$lang['account_import_upload_csv_file'] = 'Загрузіць файл CSV';
