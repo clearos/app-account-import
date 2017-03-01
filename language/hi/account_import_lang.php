eb333a25b1f185a9d5773d69ab901219189c7a2f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:01:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'खाता आयात एप्लिकेशन अल्पविराम से अलग मान (CSV) प्रारूप में एक अपलोड फ़ाइल का उपयोग खातों के थोक आयात करता है।';
+$lang['account_import_app_name'] = 'खाता आयात';
+$lang['account_import_clear'] = 'स्पष्ट';
+$lang['account_import_csv_file'] = 'सीएसवी फ़ाइल';
+$lang['account_import_csv_file_not_found'] = 'सीएसवी फ़ाइल नहीं मिला।';
+$lang['account_import_download_csv_template'] = 'डाउनलोड सीएसवी खाका';
+$lang['account_import_import_already_in_progress'] = 'आयात पहले से ही प्रगति पर है।';
+$lang['account_import_import_complete'] = 'आयात पूरा करें।';
+$lang['account_import_import_log'] = 'आयात लॉग';
+$lang['account_import_import_users'] = 'उपयोगकर्ता आयात';
+$lang['account_import_importing_user'] = 'आयात उपयोगकर्ता';
+$lang['account_import_no_data_found'] = 'कोई डेटा नहीं मिला।';
+$lang['account_import_no_entries'] = 'कोई आयात प्रविष्टियों पाए गए।';
+$lang['account_import_number_of_records'] = 'अभिलेखों की संख्या';
+$lang['account_import_start_import'] = 'आयात शुरू';
+$lang['account_import_tooltip_csv'] = 'सुनिश्चित करें कि आपका सीएसवी फाइल ठीक से प्रारूपित है - यह मूल्यों कोटेशन में संलग्न है और एक अल्पविराम ( "VALUE1", "VALUE2" आदि) द्वारा अलग होनी चाहिए।';
+$lang['account_import_unable_to_determine_running_state'] = 'यदि आयात चल रहा है यह निर्धारित करने में असमर्थ।';
+$lang['account_import_upload_csv_file'] = 'CSV फ़ाइल अपलोड करें';
