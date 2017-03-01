bf4057741aec126b637cd648f03d40c819e3d974
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:26:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'التطبيق استيراد حساب يؤدي استيراد الجزء الأكبر من حسابات باستخدام ملف مرفوع في شكل قيم مفصولة بفواصل (CSV).';
+$lang['account_import_app_name'] = 'استيراد حساب';
+$lang['account_import_clear'] = 'واضح';
+$lang['account_import_csv_file'] = 'ملف CSV';
+$lang['account_import_csv_file_not_found'] = 'ملف CSV غير موجود.';
+$lang['account_import_download_csv_template'] = 'تحميل CSV قالب';
+$lang['account_import_import_already_in_progress'] = 'استيراد قيد التقدم.';
+$lang['account_import_import_complete'] = 'استيراد كامل.';
+$lang['account_import_import_log'] = 'استيراد دخول';
+$lang['account_import_import_users'] = 'استيراد المستخدمين';
+$lang['account_import_importing_user'] = 'استيراد المستعمل';
+$lang['account_import_no_data_found'] = 'لاتوجد بيانات.';
+$lang['account_import_no_entries'] = 'لم يتم العثور على إدخالات الاستيراد.';
+$lang['account_import_number_of_records'] = 'عدد من السجلات';
+$lang['account_import_start_import'] = 'بدء الاستيراد';
+$lang['account_import_tooltip_csv'] = 'ضمان تنسيق ملف CSV بشكل صحيح - يجب أن يكون لديك قيم المغلقة في الأسعار ومفصولة بفاصلة ( "VALUE1"، "VALUE2" وما إلى ذلك).';
+$lang['account_import_unable_to_determine_running_state'] = 'غير قادر على تحديد ما إذا كان استيراد قيد التشغيل.';
+$lang['account_import_upload_csv_file'] = 'تحميل ملف CSV';
