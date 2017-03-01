3c8ee4aff4df90728f0543720a6dab2124bec273
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:02:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'A fiók Import alkalmazás elvégzi tömeges import számlák segítségével feltöltött fájlt vesszővel elválasztott értékek (CSV) formátumban.';
+$lang['account_import_app_name'] = 'fiók importálása';
+$lang['account_import_clear'] = 'Egyértelmű';
+$lang['account_import_csv_file'] = 'CSV-fájl';
+$lang['account_import_csv_file_not_found'] = 'CSV-fájl nem található.';
+$lang['account_import_download_csv_template'] = 'CSV-sablon letöltése';
+$lang['account_import_import_already_in_progress'] = 'Import már folyamatban van.';
+$lang['account_import_import_complete'] = 'Import kész.';
+$lang['account_import_import_log'] = 'import Napló';
+$lang['account_import_import_users'] = 'felhasználók importálása';
+$lang['account_import_importing_user'] = 'importáló felhasználó';
+$lang['account_import_no_data_found'] = 'Nem található adat.';
+$lang['account_import_no_entries'] = 'Nem import található bejegyzés.';
+$lang['account_import_number_of_records'] = 'Rekordok száma';
+$lang['account_import_start_import'] = 'Importálás indítása';
+$lang['account_import_tooltip_csv'] = 'Biztosítani kell a CSV-fájl helyes formázása - meg kellett volna értékeket zárt idézetek és vesszővel elválasztva ( "value1", "value2", stb).';
+$lang['account_import_unable_to_determine_running_state'] = 'Nem lehet meghatározni, ha a behozatal fut.';
+$lang['account_import_upload_csv_file'] = 'CSV-fájl feltöltése';
