fca41d4000df816c37238df7704bcad74c6e91d1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:29:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Hesab Import app virgülle ayrılmış dəyərlər (CSV) formatında qəbul faylı istifadə edərək, hesabları toplu idxal həyata keçirir.';
+$lang['account_import_app_name'] = 'hesab Import';
+$lang['account_import_clear'] = 'aydın';
+$lang['account_import_csv_file'] = 'CSV File';
+$lang['account_import_csv_file_not_found'] = 'CSV faylı tapılmadı.';
+$lang['account_import_download_csv_template'] = 'CSV Şablon Download';
+$lang['account_import_import_already_in_progress'] = 'Import davam artıq.';
+$lang['account_import_import_complete'] = 'Import tam.';
+$lang['account_import_import_log'] = 'Import Log';
+$lang['account_import_import_users'] = 'Import İstifadəçilər';
+$lang['account_import_importing_user'] = 'İthalat istifadəçi';
+$lang['account_import_no_data_found'] = 'No data tapılmadı.';
+$lang['account_import_no_entries'] = 'No idxal entries tapıldı.';
+$lang['account_import_number_of_records'] = 'Records sayı';
+$lang['account_import_start_import'] = 'Start Import';
+$lang['account_import_tooltip_csv'] = 'Sizin CSV fayl düzgün biçimlendirilmiş təmin - bu dəyərlər kotirovkaların əlavə və bir vergül ( "value1", "VALUE2" və s.) tərəfindən ayrılmış olmalıdır.';
+$lang['account_import_unable_to_determine_running_state'] = 'idxal çalışan əgər müəyyən etmək mümkün deyil.';
+$lang['account_import_upload_csv_file'] = 'CSV Fayl yüklə';
