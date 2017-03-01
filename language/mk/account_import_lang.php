5ab32049ca0feff1ac9647026ce1a679028585e5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:25:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'стан на сметката Увоз врши рефус увоз на сметки со користење на качената датотека во одвоени со запирки вредности (CSV) формат.';
+$lang['account_import_app_name'] = 'Увоз сметка';
+$lang['account_import_clear'] = 'Отстрани';
+$lang['account_import_csv_file'] = 'CSV датотека';
+$lang['account_import_csv_file_not_found'] = 'CSV датотека не е пронајдена.';
+$lang['account_import_download_csv_template'] = 'Преземете CSV Шаблон';
+$lang['account_import_import_already_in_progress'] = 'Увоз е веќе во тек.';
+$lang['account_import_import_complete'] = 'Увоз заврши.';
+$lang['account_import_import_log'] = 'увоз се Влез';
+$lang['account_import_import_users'] = 'увоз корисници';
+$lang['account_import_importing_user'] = 'Внесување на корисник';
+$lang['account_import_no_data_found'] = 'пронајдени податоци.';
+$lang['account_import_no_entries'] = 'Не се пронајдени записи увоз.';
+$lang['account_import_number_of_records'] = 'Број на рекорди';
+$lang['account_import_start_import'] = 'почеток на увозот';
+$lang['account_import_tooltip_csv'] = 'Осигурајте се дека вашиот CSV датотека е форматирана правилно - тоа треба да имаат вредности дадени во цитати и одделени со запирка ( "VALUE1", "VALUE2" и др.)';
+$lang['account_import_unable_to_determine_running_state'] = 'Не може да се утврди дали увозот се извршува.';
+$lang['account_import_upload_csv_file'] = 'Внеси CSV датотека';
