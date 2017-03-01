b501ed6a09131a9271f4ceb154c7d9f6aec6929c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:39:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'L&#39;aplicació compta d&#39;importació realitza la importació massiva de comptes utilitzant un arxiu carregat en format de valors separats per comes (CSV).';
+$lang['account_import_app_name'] = 'importació compte';
+$lang['account_import_clear'] = 'clar';
+$lang['account_import_csv_file'] = 'fitxer CSV';
+$lang['account_import_csv_file_not_found'] = 'fitxer CSV no trobat.';
+$lang['account_import_download_csv_template'] = 'Descarregar la plantilla CSV';
+$lang['account_import_import_already_in_progress'] = 'Importació ja s&#39;està realitzant.';
+$lang['account_import_import_complete'] = 'Importació completa.';
+$lang['account_import_import_log'] = 'Importa registre';
+$lang['account_import_import_users'] = 'Importa usuaris';
+$lang['account_import_importing_user'] = 'Importació d&#39;usuaris';
+$lang['account_import_no_data_found'] = 'No es van trobar dades.';
+$lang['account_import_no_entries'] = 'No s&#39;han trobat entrades de les importacions.';
+$lang['account_import_number_of_records'] = 'Nombre de registres';
+$lang['account_import_start_import'] = 'Inicia importació';
+$lang['account_import_tooltip_csv'] = 'Comproveu que el fitxer CSV té el format correcte - s&#39;ha de tenir valors tancada entre cometes i separats per una coma ( "valor1", "valor2", etc.).';
+$lang['account_import_unable_to_determine_running_state'] = 'No es pot determinar si la importació s&#39;està executant.';
+$lang['account_import_upload_csv_file'] = 'Puja un fitxer CSV';
