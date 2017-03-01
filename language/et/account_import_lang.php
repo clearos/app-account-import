f7f91ad5c958bc782136d5bfc62110d9a03acca0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:48:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Konto Import rakendus täidab hulgiimporti kontosid kasutades üleslaetud faili komaga eraldatud väärtused (CSV) vormingus.';
+$lang['account_import_app_name'] = 'konto Import';
+$lang['account_import_clear'] = 'selge';
+$lang['account_import_csv_file'] = 'CSV';
+$lang['account_import_csv_file_not_found'] = 'CSV faili ei leitud.';
+$lang['account_import_download_csv_template'] = 'Lae CSV mall';
+$lang['account_import_import_already_in_progress'] = 'Import on juba käimas.';
+$lang['account_import_import_complete'] = 'Import täielik.';
+$lang['account_import_import_log'] = 'Import Logi';
+$lang['account_import_import_users'] = 'Import Kasutajad';
+$lang['account_import_importing_user'] = 'importiv kasutaja';
+$lang['account_import_no_data_found'] = 'Andmeid ei leitud.';
+$lang['account_import_no_entries'] = 'Ei import kirjeid ei leitud.';
+$lang['account_import_number_of_records'] = 'Kirjete arv';
+$lang['account_import_start_import'] = 'Start Import';
+$lang['account_import_tooltip_csv'] = 'Tagada oma CSV fail on õigesti vormindatud - see peaks olema väärtuste suletud tsitaate ja eraldada komaga ( "value1", "value2" jne).';
+$lang['account_import_unable_to_determine_running_state'] = 'Ei saa teha kindlaks, kas import töötab.';
+$lang['account_import_upload_csv_file'] = 'Lae CSV';
