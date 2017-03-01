e0e57246775f95e798b2ec574858d9e2f72d8772
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:44:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Účet importom aplikácia vykonáva hromadný import účtov za použitia nahraný súbor s hodnotami oddelenými čiarkami (CSV) formáte.';
+$lang['account_import_app_name'] = 'importovať účet';
+$lang['account_import_clear'] = 'jasný';
+$lang['account_import_csv_file'] = 'CSV súborov';
+$lang['account_import_csv_file_not_found'] = 'CSV súbor nebol nájdený.';
+$lang['account_import_download_csv_template'] = 'Na stiahnutie vo formáte CSV šablóny';
+$lang['account_import_import_already_in_progress'] = 'Import už prebieha.';
+$lang['account_import_import_complete'] = 'Import bol dokončený.';
+$lang['account_import_import_log'] = 'import Log';
+$lang['account_import_import_users'] = 'import užívateľov';
+$lang['account_import_importing_user'] = 'import užívateľ';
+$lang['account_import_no_data_found'] = 'Neboli nájdené žiadne dáta.';
+$lang['account_import_no_entries'] = 'Neboli nájdené žiadne dovozné položky.';
+$lang['account_import_number_of_records'] = 'počet záznamov';
+$lang['account_import_start_import'] = 'spustiť import';
+$lang['account_import_tooltip_csv'] = 'Skontrolujte, či súbor CSV v správnom formáte - to by malo mať hodnoty uzavreté v úvodzovkách a oddelené čiarkou ( "hodnotou1", "hodnota2" atď).';
+$lang['account_import_unable_to_determine_running_state'] = 'Nemožno určiť, či je spustený import.';
+$lang['account_import_upload_csv_file'] = 'Nahrať súbor CSV';
