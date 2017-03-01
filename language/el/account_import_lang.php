ff05a8c1021e5c720fecf25a3d8bf589118a8617
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:55:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Η εφαρμογή Λογαριασμός Εισαγωγή εκτελεί μαζική εισαγωγή των λογαριασμών χρησιμοποιώντας ένα αρχείο φορτώθηκε σε τιμές διαχωρισμένες με κόμμα (CSV).';
+$lang['account_import_app_name'] = 'Εισαγωγή λογαριασμό';
+$lang['account_import_clear'] = 'Σαφή';
+$lang['account_import_csv_file'] = 'CSV αρχείου';
+$lang['account_import_csv_file_not_found'] = 'αρχείο CSV δεν βρέθηκε.';
+$lang['account_import_download_csv_template'] = 'Λήψη CSV Πρότυπο';
+$lang['account_import_import_already_in_progress'] = 'Εισαγωγής είναι ήδη σε εξέλιξη.';
+$lang['account_import_import_complete'] = 'Εισαγωγής πλήρης.';
+$lang['account_import_import_log'] = 'Εισαγωγές Σύνδεση';
+$lang['account_import_import_users'] = 'εισαγωγή χρηστών';
+$lang['account_import_importing_user'] = 'εισαγωγή χρήστη';
+$lang['account_import_no_data_found'] = 'Δε βρέθηκαν δεδομένα.';
+$lang['account_import_no_entries'] = 'Δεν καταχωρήσεις εισαγωγής δεν βρέθηκε.';
+$lang['account_import_number_of_records'] = 'Αριθμός Μητρώου';
+$lang['account_import_start_import'] = 'έναρξη εισαγωγής';
+$lang['account_import_tooltip_csv'] = 'Βεβαιωθείτε το αρχείο CSV έχει διαμορφωθεί σωστά - θα πρέπει να έχουν τιμές που περικλείεται σε εισαγωγικά και χωρίζονται με κόμμα ( "value1", "value2" κλπ).';
+$lang['account_import_unable_to_determine_running_state'] = 'Σε θέση να προσδιορίσει εάν η εισαγωγή εκτελείται.';
+$lang['account_import_upload_csv_file'] = 'Μεταφόρτωση αρχείου CSV';
