a4eed60419aca088bb1607d0ad5abfdca59beb7a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:57:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'એકાઉન્ટ આયાત એપ્લિકેશન અલ્પવિરામથી અલગ પડેલ કિંમતો (CSV) ફોર્મેટમાં એક અપલોડ ફાઇલની મદદથી એકાઉન્ટ્સ જથ્થાબંધ આયાત કરે છે.';
+$lang['account_import_app_name'] = 'એકાઉન્ટ આયાત';
+$lang['account_import_clear'] = 'ચોખ્ખુ';
+$lang['account_import_csv_file'] = 'CSV ફાઈલ';
+$lang['account_import_csv_file_not_found'] = 'CSV ફાઈલ મળી નથી.';
+$lang['account_import_download_csv_template'] = 'CSV ડાઉનલોડ કરો ઢાંચો';
+$lang['account_import_import_already_in_progress'] = 'આયાત પહેલેથી જ ચાલુ છે.';
+$lang['account_import_import_complete'] = 'આયાત પૂર્ણ.';
+$lang['account_import_import_log'] = 'આયાત પ્રવેશ કરો';
+$lang['account_import_import_users'] = 'આયાત વપરાશકર્તાઓ';
+$lang['account_import_importing_user'] = 'આયાત વપરાશકર્તા';
+$lang['account_import_no_data_found'] = 'કોઈ ડેટા જોવા મળે છે.';
+$lang['account_import_no_entries'] = 'કોઈ આયાત પ્રવેશો મળી આવ્યા હતા.';
+$lang['account_import_number_of_records'] = 'રેકોર્ડ સંખ્યા';
+$lang['account_import_start_import'] = 'આયાત પ્રારંભ';
+$lang['account_import_tooltip_csv'] = 'ખાતરી કરો કે તમારા CSV ફાઈલ યોગ્ય રીતે ફોર્મેટ છે - તે કિંમતો ક્વોટેશન બંધ અને એક અલ્પવિરામ ( "મૂલ્ય 1", "VALUE2" વગેરે) દ્વારા અલગ હોવી જોઇએ.';
+$lang['account_import_unable_to_determine_running_state'] = 'જો આયાત ચાલી રહ્યું છે તે નક્કી કરવા માટે અસમર્થ.';
+$lang['account_import_upload_csv_file'] = 'અપલોડ કરો CSV ફાઈલ';
