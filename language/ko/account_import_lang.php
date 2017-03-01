4556e4be8563a07e83656390393e7693ff3ac625
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:16:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = '계정 가져 오기 응용 프로그램은 쉼표로 구분 된 값 (CSV) 형식으로 업로드 된 파일을 사용하여 계정의 대량 가져 오기를 수행합니다.';
+$lang['account_import_app_name'] = '계정 가져 오기';
+$lang['account_import_clear'] = '명확한';
+$lang['account_import_csv_file'] = 'CSV 파일';
+$lang['account_import_csv_file_not_found'] = 'CSV 파일을 찾을 수 없습니다.';
+$lang['account_import_download_csv_template'] = 'CSV 템플릿 다운로드';
+$lang['account_import_import_already_in_progress'] = '가져 오기는 이미 진행 중입니다.';
+$lang['account_import_import_complete'] = '가져 오기 완료.';
+$lang['account_import_import_log'] = '가져 오기 로그인';
+$lang['account_import_import_users'] = '사용자 가져 오기';
+$lang['account_import_importing_user'] = '가져 오기 사용자';
+$lang['account_import_no_data_found'] = '데이터가 없습니다.';
+$lang['account_import_no_entries'] = '어떤 수입 항목을 찾을 수 없습니다.';
+$lang['account_import_number_of_records'] = '기록의 수';
+$lang['account_import_start_import'] = '가져 오기 시작';
+$lang['account_import_tooltip_csv'] = 'CSV 파일이 제대로 포맷되어 있는지 확인 -이 값을 따옴표로 묶어서 쉼표 ( "값 1", "값 2"등)로 구분이 있어야합니다.';
+$lang['account_import_unable_to_determine_running_state'] = '가져 오기가 실행 중인지 확인할 수 없습니다.';
+$lang['account_import_upload_csv_file'] = 'CSV 파일 업로드';
