35bcfcb148cd5d1f058c614dc379f845f191c706
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:55:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Додаток Account Імпорт здійснює масовий імпорт рахунків з використанням завантаженого файлу в форматі значення, розділені комами (CSV).';
+$lang['account_import_app_name'] = 'Імпорт облікового запису';
+$lang['account_import_clear'] = 'ясно';
+$lang['account_import_csv_file'] = 'CSV-файл';
+$lang['account_import_csv_file_not_found'] = 'CSV файл не знайдений.';
+$lang['account_import_download_csv_template'] = 'Завантажити CSV-шаблон';
+$lang['account_import_import_already_in_progress'] = 'Імпорт вже йде.';
+$lang['account_import_import_complete'] = 'Імпорт завершено.';
+$lang['account_import_import_log'] = 'імпорт Вхід';
+$lang['account_import_import_users'] = 'імпорт користувачів';
+$lang['account_import_importing_user'] = 'імпорт користувачів';
+$lang['account_import_no_data_found'] = 'Даних не знайдено.';
+$lang['account_import_no_entries'] = 'Не знайдено жодного імпорту записів.';
+$lang['account_import_number_of_records'] = 'кількість записів';
+$lang['account_import_start_import'] = 'Start Import';
+$lang['account_import_tooltip_csv'] = 'Переконайтеся, що ваш файл CSV відформатований належним чином - воно повинно мати значення, укладені в лапки і розділених комою ( "value1", "value2" і т.д.).';
+$lang['account_import_unable_to_determine_running_state'] = 'Неможливо визначити, якщо імпорт працює.';
+$lang['account_import_upload_csv_file'] = 'Завантажити файл CSV';
