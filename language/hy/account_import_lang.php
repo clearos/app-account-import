ba48cd966ea021aa877adaae59556e35bcce0b12
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:28:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Հաշվի Ներմուծում ծրագիրը իրականացնում է հիմնական ներմուծումը հաշիվների օգտագործելով վերբեռնված ֆայլը ստորակետով անջատված արժեքներ (CSV) ֆորմատով:';
+$lang['account_import_app_name'] = 'Հաշվի Ներմուծում';
+$lang['account_import_clear'] = 'պարզ';
+$lang['account_import_csv_file'] = 'CSV ֆայլը';
+$lang['account_import_csv_file_not_found'] = 'CSV ֆայլը չի ​​գտնվել:';
+$lang['account_import_download_csv_template'] = 'Ներբեռնել CSV Կաղապար';
+$lang['account_import_import_already_in_progress'] = 'Import արդեն իսկ ընթացքի մեջ է:';
+$lang['account_import_import_complete'] = 'Ներմուծել ավարտված է:';
+$lang['account_import_import_log'] = 'Ներմուծում Մուտք';
+$lang['account_import_import_users'] = 'Ներմուծել Users';
+$lang['account_import_importing_user'] = 'Importing օգտվողին';
+$lang['account_import_no_data_found'] = 'Տվյալներ չկան գտնվել:';
+$lang['account_import_no_entries'] = 'Ներմուծման գրառում են գտել:';
+$lang['account_import_number_of_records'] = 'Միավորների Records';
+$lang['account_import_start_import'] = 'Սկսել ներմուծումը';
+$lang['account_import_tooltip_csv'] = 'Համոզվեք, որ ձեր CSV ֆայլը ֆորմատավորումից հարկն - այն պետք է ունենա արժեքները կցվում է մեջբերումների եւ բաժանվել ստորակետով ( «VALUE1», «VALUE2» եւ այլն):';
+$lang['account_import_unable_to_determine_running_state'] = 'Անհնար է որոշելու, եթե ներմուծումը վազում.';
+$lang['account_import_upload_csv_file'] = 'Վերբեռնել CSV ֆայլը';
