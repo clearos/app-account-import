41fdefb58b0043e914442e4cb75e824cc60bdaa7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:51:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'programu Akaunti Import hufanya wingi kuagiza ya akaunti kwa kutumia faili kupakiwa katika format comma kutengwa maadili (CSV).';
+$lang['account_import_app_name'] = 'Import akaunti';
+$lang['account_import_clear'] = 'wazi';
+$lang['account_import_csv_file'] = 'CSV faili';
+$lang['account_import_csv_file_not_found'] = 'CSV file haikupatikana.';
+$lang['account_import_download_csv_template'] = 'Download CSV Kigezo';
+$lang['account_import_import_already_in_progress'] = 'Import ni tayari katika maendeleo.';
+$lang['account_import_import_complete'] = 'Import kamili.';
+$lang['account_import_import_log'] = 'Import Log';
+$lang['account_import_import_users'] = 'Import Watumiaji';
+$lang['account_import_importing_user'] = 'kuagiza user';
+$lang['account_import_no_data_found'] = 'Hakuna data kupatikana.';
+$lang['account_import_no_entries'] = 'Hakuna entries kuagiza walikutwa.';
+$lang['account_import_number_of_records'] = 'Idadi ya Records';
+$lang['account_import_start_import'] = 'Start Import';
+$lang['account_import_tooltip_csv'] = 'Kuhakikisha faili yako CSV ni formatted vizuri - ni lazima kuwa na maadili iliyoambatanishwa katika nukuu na kutengwa kwa koma ( "value1", "value2" nk).';
+$lang['account_import_unable_to_determine_running_state'] = 'Hawawezi kuamua kama kuagiza ni mbio.';
+$lang['account_import_upload_csv_file'] = 'Pakia CSV faili';
