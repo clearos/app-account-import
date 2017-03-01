6a6d7c303ac46b2bfa8a3b5fa17fffb200d28183
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 08:06:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'The Account Import ngwa anamde nnukwu mbubata nke akaụntụ site na iji uploaded faịlụ na rikoma iche ụkpụrụ (CSV) Ọkpụkpọ.';
+$lang['account_import_app_name'] = 'Account Import';
+$lang['account_import_clear'] = 'Clear';
+$lang['account_import_csv_file'] = 'CSV File';
+$lang['account_import_csv_file_not_found'] = 'CSV faịlụ ahụghị.';
+$lang['account_import_download_csv_template'] = 'Download CSV Template';
+$lang['account_import_import_already_in_progress'] = 'Import bụ ugbua na-enwe ọganihu.';
+$lang['account_import_import_complete'] = 'Import zuru ezu.';
+$lang['account_import_import_log'] = 'Import Log';
+$lang['account_import_import_users'] = 'Import Ọrụ';
+$lang['account_import_importing_user'] = 'Importing ọrụ';
+$lang['account_import_no_data_found'] = 'Ọ dịghị data hụrụ.';
+$lang['account_import_no_entries'] = 'Enweghị ihe ọ pụtara Ndenye ahuru.';
+$lang['account_import_number_of_records'] = 'Number nke Records';
+$lang['account_import_start_import'] = 'Malite Import';
+$lang['account_import_tooltip_csv'] = 'Hụ na gị na CSV faịlụ Ị kwesịrị ekwesị - ya kwesịrị inwe ụkpụrụ nchọ na e kwuru na iche site na a rikoma ( "value1", "value2" wdg).';
+$lang['account_import_unable_to_determine_running_state'] = 'Enweghị ike ịmata ma onye mbubata na-agba ọsọ.';
+$lang['account_import_upload_csv_file'] = 'Bulite CSV File';
