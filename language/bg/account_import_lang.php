c24485e02c65d07ce406aba5c93b13faf467a871
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 07:36:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/account_import_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['account_import_app_description'] = 'Приложението на профила Внос изпълнява насипно състояние внос на сметки, използвайки качен файл в разделени със запетая стойности (CSV) формат.';
+$lang['account_import_app_name'] = 'внос на профила';
+$lang['account_import_clear'] = 'ясно';
+$lang['account_import_csv_file'] = 'CSV файла';
+$lang['account_import_csv_file_not_found'] = 'CSV файл не е намерен.';
+$lang['account_import_download_csv_template'] = 'Изтегляне на CSV Template';
+$lang['account_import_import_already_in_progress'] = 'Внос вече е в ход.';
+$lang['account_import_import_complete'] = 'Внос пълна.';
+$lang['account_import_import_log'] = 'внос Вход';
+$lang['account_import_import_users'] = 'внос Потребители';
+$lang['account_import_importing_user'] = 'Внос на потребителя';
+$lang['account_import_no_data_found'] = 'Няма намерени данни.';
+$lang['account_import_no_entries'] = 'Не са открити записи за внос.';
+$lang['account_import_number_of_records'] = 'Брой Records';
+$lang['account_import_start_import'] = 'Старт Import';
+$lang['account_import_tooltip_csv'] = 'Уверете се, CSV файл е форматиран правилно - тя трябва да има стойности затворени в котировките и разделени със запетая ( "value1", "value2" и т.н.).';
+$lang['account_import_unable_to_determine_running_state'] = 'Не може да се определи, ако вносът е стартиран.';
+$lang['account_import_upload_csv_file'] = 'Качване на CSV файла';
